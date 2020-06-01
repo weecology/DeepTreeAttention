4cebddfcaa85db9bcdf42e5616fb006788a2dab8
bw4sz <benweinstein2010@gmail.com>
2020-06-01 07:39:40 -0700

    project skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of Hang et al. 2020 "Hyperspectral Image Classification with Attention Aided CNNs" for tree species prediction ',
+    author='Ben Weinstein - University of Florida',
+    license='MIT',
+)
