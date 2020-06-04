51e7871cbdedd37297f47b93765c4e8e7e9950dc
bw4sz <benweinstein2010@gmail.com>
2020-06-03 20:26:34 -0700

    setting up tests

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='DeepTreeAttention',
     packages=find_packages(),
     version='0.1.0',
     description='Implementation of Hang et al. 2020 "Hyperspectral Image Classification with Attention Aided CNNs" for tree species prediction ',
---
+++ tests/test_create_model.py
@@ -0,0 +1,6 @@
+#test create model
+from DeepTreeAttention.models import create_model
+
+def test_create_model():
+    model = create_model.model()
+    
