c9e7f696d9f1f0b1fb4963c61a363de4196663a1
bw4sz <benweinstein2010@gmail.com>
2020-06-04 12:24:33 -0700

    basic model structure, but shape is still incorrect

--- DeepTreeAttention/models/create_model.py
+++ DeepTreeAttention/models/create_model.py
@@ -7,14 +7,17 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import layers
 
 
-def conv_module(x, K, kX, kY, stride, chanDim, padding="same"):
+def conv_module(x, K, kX=3, kY=3, chanDim=-1, padding="same", maxpool=False):
     """
+    Basic convolutional block with batch norm and optional max pooling
     """
     # define a CONV => BN => RELU pattern
-    x = Conv2D(K, (kX, kY), strides=stride, padding=padding)(x)
-    x = BatchNormalization(axis=chanDim)(x)
-    x = Activation("relu")(x)
-    # return the block
+    x = layers.Conv2D(K, (kX, kY), padding=padding)(x)
+    x = layers.BatchNormalization(axis=chanDim)(x)
+    x = layers.Activation("relu")(x)
+    if maxpool:
+        x = layers.MaxPool2D()(x)
+        
     return x
 
 
@@ -23,68 +26,72 @@ def concat_attention(conv_layers, attention_layers):
     Element-wise multiplication and padding
     """
 
-    return conv_layers
+    return concat_layers
 
 
-def conv_and_attention_submodel(x, submodel):
+def conv_and_attention_submodel(x, K, submodel, attention=True, maxpool=False):
     """Perform a set of convultions and attention and concat
     Args:
         x: functional keras model layers
+        K: convolutional filters
         submodel: either "spectral" or "spatial" to get the right attention layer
+        maxpool: whether convolutional layers have a maxpool layer
     Returns:
         x: functional keras model layers
     """
 
     #Layers are a convoultional layer, an attention pooling layer and a concat
-    conv_layers = conv_module(x, K, kX, kY, stride, chanDim)
-    if submodel == "spectral":
-        attention_layers = spectral_attention(conv_layer)
-    elif submodel == "spatial":
-        attention_layers = spatial_attention(conv_layer)
-    else:
-        raise ValueError("submodel must be either 'spectral' or 'spatial'")
+    x = conv_module(x, K, maxpool=maxpool)
+    #add attention
+    if attention:
+        if submodel == "spectral":
+            attention_layers = spectral_attention(x)
+        elif submodel == "spatial":
+            attention_layers = spatial_attention(x)
+        else:
+            raise ValueError("submodel must be either 'spectral' or 'spatial'")
 
-    #combine
-    x = concat_attention(conv_layers, attention_layers)
+        #combine
+        x = concat_attention(x, attention_layers)
 
     return x
 
 
-def spatial_network(x):
+def spatial_network(x, attention=False, classes=2):
     """
     Learn spatial features with convolutional and attention pooling layers
     """
     #First submodel is 32 filters
-    x = conv_and_attention_submodel(x, submodel="spatial")
+    x = conv_and_attention_submodel(x, K=32, submodel="spatial", attention=attention)
 
-    #Second submodel is 64 filters
-    x = conv_and_attention_submodel(x, submodel="spatial")
+    #Second submodel is 64 filters - 2nd gets maxpool
+    x = conv_and_attention_submodel(x, K=64, submodel="spatial", attention=attention, maxpool=True)
 
-    #Third submodel is 128 filters
-    x = conv_and_attention_submodel(x, submodel="spatial")
+    #Third submodel is 128 filters - 3rd gets maxpool
+    x = conv_and_attention_submodel(x, K=128, submodel="spatial", attention=attention, maxpool=True)
 
     #This is still unclear, is it flatten and dense or just dense?
-    x = layers.Dense(classes, activation="relu")
+    x = layers.Dense(classes, activation="softmax")(x)
 
     return x
 
 
-def spectral_network(x, classes=2):
+def spectral_network(x, attention=False, classes=2):
     """
     Learn cross band spectral features with a set of convolutions and spectral pooling attention layers
     """
 
-    #First submodel is 32 filters
-    x = conv_and_attention_submodel(x, submodel="spectral")
+    #First submodel is 32 filter
+    x = conv_and_attention_submodel(x, K=32, submodel="spectral", attention=attention)
 
     #Second submodel is 64 filters
-    x = conv_and_attention_submodel(x, submodel="spectral")
+    x = conv_and_attention_submodel(x, K=64, submodel="spectral", attention=attention, maxpool=True)
 
     #Third submodel is 128 filters
-    x = conv_and_attention_submodel(x, submodel="spectral")
+    x = conv_and_attention_submodel(x, K=128, submodel="spectral", attention=attention, maxpool=True)
 
     #This is still unclear, is it flatten and dense or just dense?
-    x = layers.Dense(classes, activation="relu")
+    x = layers.Dense(classes, activation="softmax")(x)
 
     return x
 
@@ -101,10 +108,39 @@ def spatial_attention(x):
     return x
 
 
-def weighed_consensus(spatial_layers, spectral_layer):
+def _weighted_sum(x):
+    return tf.keras.backend.sum(x[0] * tf.keras.backend.expand_dims(x[1], -1), axis=1, keepdims=True)
 
+class WeightedSum(layers.Layer):
+    """A custom keras layer to learn a weighted sum of tensors"""
+    def __init__(self, **kwargs):
+        super(WeightedSum, self).__init__(**kwargs)
+
+    def build(self, input_shape=1):
+        self.a = self.add_weight(
+            name='a',
+            shape=(),
+            initializer='ones',
+            dtype='float32',
+            trainable=True,
+        )
+        super(WeightedSum, self).build(input_shape)
+        
+    def call(self, model_outputs):
+        return self.a * model_outputs[0] + (1 - self.a) * model_outputs[1]
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+    
+def submodule_consensus(spatial_layers, spectral_layers, weighted=True):
+    """Learned weighted sum among layers"""
+    
+    if weighted:
+        x = WeightedSum()([spatial_layers, spectral_layers])
+    else:
+        x = layers.Average()([spatial_layers, spectral_layers])
+    
     return x
-
 
 def model(height=48, width=11, depth=11, classes=2):
     """
@@ -116,9 +152,11 @@ def model(height=48, width=11, depth=11, classes=2):
     spatial_layers = spatial_network(inputs)
 
     #spectral network
-    spectral_layer = spectral_network(inputs)
-
-    outputs = weighted_consensus(spatial_layers, spectral_layer)
+    spectral_layers = spectral_network(inputs)
+    
+    #Still need to learn weights
+    outputs = submodule_consensus(spatial_layers, spectral_layers, weighted=False)
+    
     model = Model(inputs=inputs, outputs=outputs, name="DeepTreeAttention")
 
     return model
