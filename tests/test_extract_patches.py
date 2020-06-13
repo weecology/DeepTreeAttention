1d6f94e217cbd4b01c46dce681185512e2fc8d5f
bw4sz <benweinstein2010@gmail.com>
2020-06-13 15:39:14 -0700

    additional test

--- tests/test_extract_patches.py
+++ tests/test_extract_patches.py
@@ -7,15 +7,25 @@ from DeepTreeAttention.generators import extract_patches
 @pytest.fixture()
 def image():
     # create fake image input (only shape is used anyway) # logic from https://github.com/fizyr/tf-retinanet/blob/master/tests/layers/test_misc.py
-    image = np.zeros((10, 10, 48), dtype=tf.keras.backend.floatx())
+    image = np.random.rand(10, 10, 48)
 
     return image    
 
 def test_extract_patches(image):
     """Extract patches and restitch"""
-    patches = extract_patches.extract_patches(image, width=2, height=2)
-    patches = tf.reshape(patches,[-1,2,2,48])
+    height = 2
+    width = 2
+    
+    patches = extract_patches.extract_patches(image, width=width, height=height)
+    patches = tf.reshape(patches,[-1,width,height,48])
     
     assert len(patches) == 25
-    assert patches[0,].shape == (2, 2, 48)
+    assert patches[0,].shape == (width, height, 48)
+    
+    #reconstruct
+    rows = tf.split(patches,image.shape[0]//height,axis=0)
+    rows = [tf.concat(tf.unstack(x),axis=1) for x in rows] 
+    
+    reconstructed = tf.concat(rows,axis=0)   
+    assert np.array_equal(image, reconstructed.numpy())
     
