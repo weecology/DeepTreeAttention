2a3e3f3a1176d89dee2dfd0ba72c82a5b7022a33
bw4sz <benweinstein2010@gmail.com>
2020-06-15 19:16:50 -0700

    test main classes

---
+++ tests/test_main.py
@@ -0,0 +1,88 @@
+#Test main
+import pytest
+import os
+import rasterio
+import numpy as np
+from DeepTreeAttention import main
+
+@pytest.fixture()
+def training_raster(tmp_path):
+    fn = os.path.join(tmp_path,"training.tif")
+    #Create a raster that looks data
+    arr = np.random.rand(4, 601,2369).astype(np.float)
+    
+    #hard coded from Houston 2018 ground truth
+    new_dataset = rasterio.open(fn, 'w', driver='GTiff',
+                                height = arr.shape[1], width = arr.shape[2],
+                                count=arr.shape[0], dtype=str(arr.dtype),
+                                crs=rasterio.crs.CRS.from_epsg("26915"),
+                                transform=rasterio.transform.from_bounds(272056.0, 3289689.0, 274440.0, 3290290.0, 4768, 1202))
+    
+    new_dataset.write(arr)
+    new_dataset.close()
+
+    return fn
+
+
+@pytest.fixture()
+def ground_truth_raster(tmp_path):
+    fn = os.path.join(tmp_path,"ground_truth.tif")
+    #Create a raster that looks data (smaller)
+    arr = np.random.randint(20,size=(1, 601,2369)).astype(np.uint8)
+    #hard coded from Houston 2018 ground truth
+    new_dataset = rasterio.open(fn, 'w', driver='GTiff',
+                                height = arr.shape[1], width = arr.shape[2],
+                                count=arr.shape[0], dtype=str(arr.dtype),
+                                crs=rasterio.crs.CRS.from_epsg("26915"),
+                                transform=rasterio.transform.from_bounds(272056.0, 3289689.0, 274440.0, 3290290.0, 4768, 1202))
+    
+    new_dataset.write(arr)
+    new_dataset.close()
+
+    return fn
+
+@pytest.fixture()
+def test_config(training_raster, ground_truth_raster):
+    config = {}
+    train_config = { }
+    train_config["sensor_path"] = training_raster
+    train_config["ground_truth_path"] = ground_truth_raster
+    train_config["batch_size"] = 10
+    train_config["epochs"] = 1
+    train_config["steps"] = 2
+    train_config["crop_height"] = 11
+    train_config["crop_width"] = 11
+    train_config["sensor_channels"] = 4
+    
+    config["train"] = train_config
+    
+    return config
+    
+def test_AttentionModel(test_config):
+    #Create class
+    mod = main.AttentionModel()    
+    #Replace config for testing env
+    for key, value in test_config.items():
+        for nested_key, nested_value in value.items():
+            mod.config[key][nested_key] = nested_value
+    
+    assert os.path.exists(mod.config["train"]["sensor_path"])
+
+    #Create
+    mod.create(classes=20, weighted_sum=False)
+
+    #initial weights
+    initial_weight = mod.model.layers[1].get_weights()
+    
+    #train
+    mod.train()
+    
+    final_weight = mod.model.layers[1].get_weights()
+    
+    #assert training took place
+    assert not np.array_equal(final_weight,initial_weight)
+    
+    
+    
+    
+    
