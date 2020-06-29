899326efe5f2f7cbf0c86dfb5887a30e97627a3d
bw4sz <benweinstein2010@gmail.com>
2020-06-28 20:53:04 -0700

    generate to config tfrecord location

--- experiments/generate.py
+++ experiments/generate.py
@@ -7,7 +7,5 @@ import os
 att = AttentionModel()
 
 #get root dir full path
-topdir = os.path.dirname(os.path.dirname(ROOT))
-tfrecord_dir = os.path.join(topdir,"data/processed")
-tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir=tfrecord_dir)
+tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir=att.config["train"]["tfrecords"])
 print("Created {} records:{}...".format(len(tfrecords),tfrecords[0:3]))
