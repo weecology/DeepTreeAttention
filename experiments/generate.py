f097813ad3d470c65b6c79be38eb2b64d219e901
bw4sz <benweinstein2010@gmail.com>
2020-06-28 20:09:48 -0700

    Self contained location of root dir

--- experiments/generate.py
+++ experiments/generate.py
@@ -1,8 +1,13 @@
 #Generate tfrecords
 from DeepTreeAttention.main import AttentionModel
+from DeepTreeAttention import __file__ as ROOT
 from DeepTreeAttention.generators import make_dataset
+import os
 
 att = AttentionModel()
 
-tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir="../data/processed/")
+#get root dir full path
+topdir = os.path.dirname(os.path.dirname(ROOT))
+tfrecord_dir = os.path.join(topdir,"data/processed")
+tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir=tfrecord_dir)
 print("Created {} records:{}...".format(len(tfrecords),tfrecords[0:3]))
