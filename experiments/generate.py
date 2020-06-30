6361e4c5eda907d83643cfd789c0bef78be3fe2a
bw4sz <benweinstein2010@gmail.com>
2020-06-29 18:23:01 -0700

    Try to speed up GPU utilization

--- experiments/generate.py
+++ experiments/generate.py
@@ -9,5 +9,5 @@ att = AttentionModel()
 
 #get root dir full path
 client = start(cpus=30, mem_size="10GB")
-tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir=att.config["train"]["tfrecords"],use_dask=True,client=client)
+tfrecords = make_dataset.generate(att.config["train"]["sensor_path"], att.config["train"]["ground_truth_path"], savedir=att.config["train"]["tfrecords"],use_dask=True,client=client, chunk_size=5000)
 print("Created {} records:{}...".format(len(tfrecords),tfrecords[0:3]))
