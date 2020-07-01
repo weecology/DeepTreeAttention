7a076130285b999a5bbed50a83dfdc14cd3f3991
bw4sz <benweinstein2010@gmail.com>
2020-07-01 17:03:46 -0400

    manually set parallel calls

--- DeepTreeAttention/generators/make_dataset.py
+++ DeepTreeAttention/generators/make_dataset.py
@@ -282,15 +282,15 @@ def tf_dataset(tfrecords,
     ignore_order = tf.data.Options()
     ignore_order.experimental_deterministic = False
     
-    dataset = tf.data.TFRecordDataset(tfrecords, num_parallel_reads=5)
+    dataset = tf.data.TFRecordDataset(tfrecords, num_parallel_reads=AUTO)
     dataset = dataset.with_options(ignore_order)
         
     #if shuffle:    
         #dataset = dataset.shuffle(buffer_size=AUTO)
     if train:
-        dataset = dataset.map(create_tfrecords._train_parse_, num_parallel_calls= AUTO)
+        dataset = dataset.map(create_tfrecords._train_parse_, num_parallel_calls=100)
     else:
-        dataset = dataset.map(create_tfrecords._predict_parse_, num_parallel_calls= AUTO)
+        dataset = dataset.map(create_tfrecords._predict_parse_, num_parallel_calls=100)
     #batch
     dataset = dataset.batch(batch_size=batch_size)
     dataset = dataset.prefetch(buffer_size=AUTO)
