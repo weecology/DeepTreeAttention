afb81aa039b6747880811444d319c8bf5cac55da
bw4sz <benweinstein2010@gmail.com>
2020-06-30 19:56:27 -0700

    just train on spatial network

--- DeepTreeAttention/models/single_conv.py
+++ DeepTreeAttention/models/single_conv.py
@@ -6,12 +6,7 @@ def create_model(height=11, width=11, channels=48, classes=2):
     """
     input_shape = (height, width, channels)
     inputs = layers.Input(shape=input_shape)
-
-    #spatial subnetwork
-    x = conv_module(inputs, K=32)
-    x = layers.Flatten()(x)
-    outputs = layers.Dense(classes, activation="softmax", name="softmax")(x)
-    
-    model = Model(inputs=inputs, outputs=outputs, name="DeepTreeAttention")
+    spatial_layers = spatial_network(inputs, classes=classes)
+    model = Model(inputs=inputs, outputs=spatial_layers, name="DeepTreeAttention_spatial")
 
     return model
