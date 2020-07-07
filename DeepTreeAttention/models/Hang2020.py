b41dec19973c284453e638bad12544cc7fd55890
bw4sz <benweinstein2010@gmail.com>
2020-07-07 09:59:55 -0700

    first pass at spatial attention layers

--- DeepTreeAttention/models/Hang2020.py
+++ DeepTreeAttention/models/Hang2020.py
@@ -7,19 +7,20 @@ def create_model(height=11, width=11, channels=48, classes=2, weighted_sum=False
     input_shape = (height, width, channels)
     inputs = layers.Input(shape=input_shape)
 
-    #spatial subnetwork
+    #spatial subnetwork and weak attention classifications
     spatial_layers = spatial_network(inputs, classes=classes)
 
     #spectral network
     spectral_layers = spectral_network(inputs, classes=classes)
 
     #Learn weighted average
-    outputs = submodule_consensus(spatial_layers,
+    combined_output = submodule_consensus(spatial_layers,
                                   spectral_layers,
                                   weighted_sum=weighted_sum)
 
-    #outputs = layers.Average()([spatial_layers, spectral_layers])
-
+    #Output layers
+    outputs = combined_output
+    
     model = Model(inputs=inputs, outputs=outputs, name="DeepTreeAttention")
 
     return model
