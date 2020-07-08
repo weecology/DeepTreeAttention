e438132a2c0b61cfcffdf37e0cfc495a8594dd72
bw4sz <benweinstein2010@gmail.com>
2020-07-08 13:43:27 -0700

    callback val loss needs to be verbose

--- DeepTreeAttention/callbacks/callbacks.py
+++ DeepTreeAttention/callbacks/callbacks.py
@@ -1,7 +1,10 @@
 #Callbacks
 """Create training callbacks"""
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+import numpy as np
+from keras.callbacks import Callback
+from sklearn.metrics import f1_score
 
 def create():
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=0.0001, verbose=1)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=0, min_lr=0.0001, verbose=1)
     return [reduce_lr]
