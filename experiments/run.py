7e6f10ba86763e1a231b8028bf5ae20d6a644be2
bw4sz <benweinstein2010@gmail.com>
2020-07-27 16:46:58 -0700

    turn off class weight

--- experiments/run.py
+++ experiments/run.py
@@ -35,12 +35,12 @@ experiment.log_parameter("Train subnetworks", True)
 with experiment.context_manager("spatial_subnetwork"):
     model.read_data(mode="submodel",validation_split=True)    
     class_weight = model.calc_class_weight()    
-    model.train(submodel="spatial", class_weight=class_weight)
+    model.train(submodel="spatial", class_weight=None)
 
 with experiment.context_manager("spectral_subnetwork"):
     model.read_data(mode="submodel",validation_split=True)   
     class_weight = model.calc_class_weight()    
-    model.train(submodel="spectral", class_weight=class_weight)
+    model.train(submodel="spectral", class_weight=None)
         
 #Train full model
 model.read_data(validation_split=True)    
