79c8224c2c9e3c4efd094c8dc95afbccba283bdc
bw4sz <benweinstein2010@gmail.com>
2020-07-30 09:33:02 -0700

    less patient

--- DeepTreeAttention/callbacks/callbacks.py
+++ DeepTreeAttention/callbacks/callbacks.py
@@ -7,8 +7,8 @@ from datetime import datetime
     
 def create(log_dir=None):
     reduce_lr = ReduceLROnPlateau(monitor='val_loss',
-                                  factor=0.5,
-                                  patience=20,
+                                  factor=0.2,
+                                  patience=5,
                                   min_lr=0.00001,
                                   verbose=1)
     
