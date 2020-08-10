4dbfca4ae6fb1a4c237a3f3abeb75b26af1adb2e
bw4sz <benweinstein2010@gmail.com>
2020-08-10 15:50:07 -0700

    refactored the generation to outside class methods

--- experiments/Trees/run.py
+++ experiments/Trees/run.py
@@ -1,7 +1,7 @@
 #Experiment
 from comet_ml import Experiment
 from datetime import datetime
-from DeepTreeAttention.main import AttentionModel
+from DeepTreeAttention.trees import AttentionModel
 from DeepTreeAttention import species
 from DeepTreeAttention.utils import metrics, resample, start_cluster
 from DeepTreeAttention.visualization import visualize
