56e42749fa93740e854aa2831aec38030780cb4b
bw4sz <benweinstein2010@gmail.com>
2020-09-24 10:11:44 -0700

    add a metadata parser

--- DeepTreeAttention/models/metadata.py
+++ DeepTreeAttention/models/metadata.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Dense
 
 def metadata_model(classes):
     # create model
-    metadata_inputs = layers.Input(shape=(1,))
+    metadata_inputs = tensorflow.keras.layers.Input(shape=(1,))
     x = Dense(classes, input_dim=1, activation='relu')(metadata_inputs)
     x = Dense(classes, input_dim=1, activation='relu')(x)
     output = Dense(classes, activation="softmax")(x)
