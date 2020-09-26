a228ee74a6793cc6a22c0eb541b30a42bbe46a91
bw4sz <benweinstein2010@gmail.com>
2020-09-26 07:24:47 -0700

    callback isn't running

--- DeepTreeAttention/visualization/visualize.py
+++ DeepTreeAttention/visualization/visualize.py
@@ -75,7 +75,11 @@ def site_confusion(y_true, y_pred, sites):
         #If not correctly predicted
         if not value == y_pred[index]:
                 correct_sites = site_lists[y_true[index]]
-                incorrect_site = site_lists[y_pred[index]]
+                try:
+                    incorrect_site = site_lists[y_pred[index]]
+                except Exception as e:
+                    print(e)
+                    continue
             
             #Do they co-occur?
             site_overlap = any([site in incorrect_site for site in correct_sites])
