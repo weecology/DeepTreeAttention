197d89193d985dd48495a08d7378350c1d77577c
bw4sz <benweinstein2010@gmail.com>
2020-09-26 09:56:03 -0700

    try no dropout

--- DeepTreeAttention/models/layers.py
+++ DeepTreeAttention/models/layers.py
@@ -222,12 +222,12 @@ def submodule_consensus(spatial_layers, spectral_layers, weighted_sum=True):
 def metadata_layer(metadata, sensor_softmax, classes):
     """Learn from a metadata layer and combined with spatial/spectral information. The combined model from Terry et al. 2020 Methods in E&E"""
     x = layers.Dense(classes, activation="relu")(metadata)
-    x = layers.Dropout(0.2)(x)
+    #x = layers.Dropout(0.2)(x)
     x = layers.Dense(classes, activation="relu")(x)
-    x = layers.Dropout(0.2)(x)    
+    #x = layers.Dropout(0.2)(x)    
     x = layers.Dense(classes, activation="softmax")(x)
     x = layers.Multiply()([x, sensor_softmax])
     
-    return sensor_softmax
+    return x
     
     
