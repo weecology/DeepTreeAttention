2e3039c55af19a3491062ef1f8b3c1e845227fd8
bw4sz <benweinstein2010@gmail.com>
2020-09-27 14:51:27 -0700

    just checking sensor only

--- DeepTreeAttention/models/Hang2020_geographic.py
+++ DeepTreeAttention/models/Hang2020_geographic.py
@@ -22,6 +22,6 @@ def create_model(height=11, width=11, channels=48, classes=2, sites=23, weighted
                                           spectral_attention_outputs[2],
                                           weighted_sum=weighted_sum)
     
-    combined_softmax = metadata_layer(metadata_inputs, sensor_softmax, classes) 
+    #combined_softmax = metadata_layer(metadata_inputs, sensor_softmax, classes) 
 
-    return sensor_inputs, metadata_inputs, combined_softmax, spatial_attention_outputs, spectral_attention_outputs
+    return sensor_inputs, metadata_inputs, sensor_softmax, spatial_attention_outputs, spectral_attention_outputs
