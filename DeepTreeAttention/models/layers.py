c33998a2c841ca61ecd5cf704c4a41ceb5c9945d
bw4sz <benweinstein2010@gmail.com>
2020-09-28 14:31:44 -0700

    deeper layers no dropout, overfitting is not the problem, not enough learning

--- DeepTreeAttention/models/layers.py
+++ DeepTreeAttention/models/layers.py
@@ -221,10 +221,8 @@ def submodule_consensus(spatial_layers, spectral_layers, weighted_sum=True):
 
 def metadata_layer(metadata, classes):
     """Learn from a metadata layer and combined with spatial/spectral information. The combined model from Terry et al. 2020 Methods in E&E"""
-    x = layers.Dense(classes, activation="relu")(metadata)
-    #x = layers.Dropout(0.2)(x)
+    x = layers.Dense(classes*4, activation="relu")(metadata)
     x = layers.Dense(classes*4, activation="relu")(x)    
-    x = layers.Dropout(0.2)(x)    
     x = layers.Dense(classes, activation="softmax")(x)
     
     return x
@@ -233,7 +231,6 @@ def merge_softmax(layers_to_merge, classes):
     """Merge a series of layers and learn an ensemble softmax"""
     x = layers.Concatenate()(layers_to_merge)
     x = layers.Dense(classes, activation="relu")(x)
-    #x = layers.Dropout(0.2)(x)        
     x = layers.Dense(classes, activation="softmax")(x)
     
     return x
