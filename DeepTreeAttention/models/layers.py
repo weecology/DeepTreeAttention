6d3d3efad45ccc7c86478f1a567cadbfd9abf639
bw4sz <benweinstein2010@gmail.com>
2020-09-29 12:34:30 -0700

    learn from element multiplication

--- DeepTreeAttention/models/layers.py
+++ DeepTreeAttention/models/layers.py
@@ -228,7 +228,7 @@ def metadata_layer(metadata, classes):
     
 def merge_softmax(layers_to_merge, classes):
     """Merge a series of layers and learn an ensemble softmax"""
-    x = layers.Multiply(layers_to_merge)
+    x = layers.Multiply()(layers_to_merge)
     x = layers.Dense(classes*2, activation="relu")(x)
     x = layers.Dense(classes, activation="softmax")(x)
     
