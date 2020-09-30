6c8d6a161e5cdf726f12ac4d3ca32cb83dd904cb
bw4sz <benweinstein2010@gmail.com>
2020-09-30 11:13:38 -0700

    shallower

--- tests/test_Hang2020_geographic.py
+++ tests/test_Hang2020_geographic.py
@@ -15,7 +15,7 @@ def image():
 #Test full model makes the correct number of predictions.
 @pytest.mark.parametrize("classes", [3, 10, 20])
 def test_model(image, classes):
-    sensor_inputs, metadata_inputs, combined_output, spatial, spectral = create_model(classes=classes)    
+    sensor_inputs, metadata_inputs, metadata_outputs, combined_output, spatial, spectral = create_model(classes=classes)    
     model = tf.keras.Model(inputs=[sensor_inputs, metadata_inputs], outputs=combined_output)
     prediction = model.predict(image)
     prediction.shape == (1, classes)
