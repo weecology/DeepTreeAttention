7764c70cd69b3dc52349ae6562bb5fe781c766af
bw4sz <benweinstein2010@gmail.com>
2020-10-16 13:17:03 -0700

    submit RGB norm

--- experiments/Trees/run_RGB.py
+++ experiments/Trees/run_RGB.py
@@ -19,7 +19,7 @@ experiment.add_tag("RGB")
 ##Train
 
 #Train see config.yml for tfrecords path with weighted classes in cross entropy
-model.read_data("RGB_train")
+model.read_data()
 class_weight = model.calc_class_weight()
 
 ##Train subnetwork
@@ -27,18 +27,18 @@ experiment.log_parameter("Train subnetworks", True)
 with experiment.context_manager("RGB_spatial_subnetwork"):
     print("Train RGB spatial subnetwork")
     model.read_data(mode="RGB_submodel")
-    model.train(submodel="spatial", sensor="rgb",class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
+    model.train(submodel="spatial", sensor="RGB",class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
 
 with experiment.context_manager("RGB_spectral_subnetwork"):
     print("Train RGB spectral subnetwork")    
     model.read_data(mode="RGB_submodel")   
-    model.train(submodel="spectral", sensor="rgb", class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
+    model.train(submodel="spectral", sensor="RGB", class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
         
 #Train full model
 with experiment.context_manager("RGB_model"):
     experiment.log_parameter("Class Weighted", True)
     model.read_data(mode="RGB_train")
-    model.train(class_weight=class_weight, sensor="rgb", experiment=experiment)
+    model.train(class_weight=class_weight, sensor="RGB", experiment=experiment)
     
     #Get Alpha score for the weighted spectral/spatial average. Higher alpha favors spatial network.
     if model.config["train"]["RGB"]["weighted_sum"]:
