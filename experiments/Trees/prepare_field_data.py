d64ef779ddc8b80299ba0cb6b93965785a389247
bw4sz <benweinstein2010@gmail.com>
2020-10-27 10:00:17 -0700

    min diff is now an arg of the config, cleaning test data

--- experiments/Trees/prepare_field_data.py
+++ experiments/Trees/prepare_field_data.py
@@ -464,11 +464,11 @@ if __name__ == "__main__":
     
     lookup_glob = "/orange/ewhite/NeonData/**/CanopyHeightModelGtif/*.tif"
     
-    #Create train test split
-    create_training_shp.train_test_split(ROOT, lookup_glob, min_diff=config["train"]["min_height_diff"])
-        
     #Read config from top level dir
     config = parse_yaml("{}/conf/tree_config.yml".format(ROOT))
+    
+    #Create train test split
+    create_training_shp.train_test_split(ROOT, lookup_glob, min_diff=config["train"]["min_height_diff"])
     
     #train data
     main(
