edc8a5a5d1123fc8537e0044d6ba023dabdf37ce
bw4sz <benweinstein2010@gmail.com>
2020-10-28 10:01:53 -0700

    single dask client with more mem per worker

--- experiments/Trees/prepare_field_data.py
+++ experiments/Trees/prepare_field_data.py
@@ -418,7 +418,6 @@ def main(
             box_indexes.extend(plot_box_index)
             
     #close client
-    client.close()
     if shuffle:
         z = list(zip(HSI_crops, RGB_crops, sites, heights, elevations, box_indexes, labels))
         random.shuffle(z)
