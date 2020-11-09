4740be326bd24a5843e90d76dc17610be522c483
bw4sz <benweinstein2010@gmail.com>
2020-11-09 09:27:28 -0800

    working on RGB only

--- experiments/Trees/run_RGB.py
+++ experiments/Trees/run_RGB.py
@@ -19,28 +19,36 @@ experiment.add_tag("RGB")
 ##Train
 
 #Train see config.yml for tfrecords path with weighted classes in cross entropy
-model.read_data()
-class_weight = model.calc_class_weight()
-
 ##Train subnetwork
 experiment.log_parameter("Train subnetworks", True)
 with experiment.context_manager("RGB_spatial_subnetwork"):
     print("Train RGB spatial subnetwork")
     model.read_data(mode="RGB_submodel")
-    model.train(submodel="spatial", sensor="RGB",class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
+    model.train(submodel="spatial", sensor="RGB", experiment=experiment)
 
 with experiment.context_manager("RGB_spectral_subnetwork"):
     print("Train RGB spectral subnetwork")    
     model.read_data(mode="RGB_submodel")   
-    model.train(submodel="spectral", sensor="RGB", class_weight=[class_weight, class_weight, class_weight], experiment=experiment)
+    model.train(submodel="spectral", sensor="RGB", experiment=experiment)
         
 #Train full model
 with experiment.context_manager("RGB_model"):
-    experiment.log_parameter("Class Weighted", True)
     model.read_data(mode="RGB_train")
-    model.train(class_weight=class_weight, sensor="RGB", experiment=experiment)
+    model.train(sensor="RGB", experiment=experiment)
     
     #Get Alpha score for the weighted spectral/spatial average. Higher alpha favors spatial network.
     if model.config["train"]["RGB"]["weighted_sum"]:
         estimate_a = model.RGB_model.get_layer("weighted_sum").get_weights()
         experiment.log_metric(name="spatial-spectral weight", value=estimate_a[0][0])
+        
+#Load RGB model
+model.HSI_model = load_model("{}/HSI_model.h5".format(model.config["train"]["checkpoint_dir"]), custom_objects={"WeightedSum": WeightedSum})     
+model.metadata_model = load_model("{}/metadata_model.h5".format(model.config["train"]["checkpoint_dir"]), compile=False)  
+
+with experiment.context_manager("ensemble"):    
+    print("Train Ensemble")
+    model.ensemble(freeze=model.config["train"]["ensemble"]["freeze"], experiment=experiment)
+
+#Final score, be absolutely sure you get all the data, feed slowly in batches of 1
+final_score = model.ensemble_model.evaluate(model.val_split.unbatch().batch(1))    
+experiment.log_metric("Ensemble Accuracy", final_score[1])
