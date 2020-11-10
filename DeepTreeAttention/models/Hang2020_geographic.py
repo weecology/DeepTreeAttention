4c28200e92af21fcdb170c600204d584b9cc0546
bw4sz <benweinstein2010@gmail.com>
2020-11-10 17:29:01 -0500

    valid scope name

--- DeepTreeAttention/models/Hang2020_geographic.py
+++ DeepTreeAttention/models/Hang2020_geographic.py
@@ -110,7 +110,7 @@ def learned_ensemble(RGB_model, HSI_model, metadata_model, classes, freeze=True)
     stripped_metadata = tf.keras.Model(inputs=metadata_model.inputs, outputs = normalized_metadata)
     
     #concat and learn ensemble weights
-    merged_layers = WeightedSum(name="HSI_meta weighted sum")([stripped_HSI_model.output, stripped_metadata.output])    
+    merged_layers = WeightedSum(name="HSI_meta_weighted_sum")([stripped_HSI_model.output, stripped_metadata.output])    
     merged_layers = layers.Dropout(0.7)(merged_layers)
     ensemble_softmax = layers.Dense(classes,name="ensemble_learn",activation="softmax")(merged_layers)
 
