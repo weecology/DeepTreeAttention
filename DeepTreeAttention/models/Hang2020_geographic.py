d79df2afcf61c2e4da9f22d6f3e46d97da45a432
bw4sz <benweinstein2010@gmail.com>
2020-11-09 18:43:27 -0800

    add after concat of HSI

--- DeepTreeAttention/models/Hang2020_geographic.py
+++ DeepTreeAttention/models/Hang2020_geographic.py
@@ -111,7 +111,8 @@ def learned_ensemble(RGB_model, HSI_model, metadata_model, classes, freeze=True)
     stripped_metadata = tf.keras.Model(inputs=metadata_model.inputs, outputs = normalized_metadata)
     
     #concat and learn ensemble weights
-    merged_layers = layers.Concatenate(name="submodel_concat")([stripped_HSI_model.output, stripped_RGB_model.output, stripped_metadata.output])    
+    merged_layers = layers.Concatenate(name="submodel_concat")([stripped_HSI_model.output, stripped_metadata.output])    
+    merged_layers = layers.Add()([merged_layers, stripped_RGB_model.output])
     merged_layers = layers.Dropout(0.3)(merged_layers)
     ensemble_softmax = layers.Dense(classes,name="ensemble_learn",activation="softmax")(merged_layers)
 
