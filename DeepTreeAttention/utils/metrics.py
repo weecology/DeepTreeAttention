17e9ca97b60f024d0d5a1189e9394991d1391a30
bw4sz <benweinstein2010@gmail.com>
2020-11-11 10:26:08 -0800

    Add within site callbacks

--- DeepTreeAttention/utils/metrics.py
+++ DeepTreeAttention/utils/metrics.py
@@ -1,8 +1,84 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.metrics import f1_score
+import geopandas as gpd
 
+def site_confusion(y_true, y_pred, site_lists):
+    """What proportion of misidentified species come from the same site?
+    Args: 
+        y_true: string values of true labels
+        y_pred: string values or predicted labels
+        site_lists: dict of site labels for each string label taxonID -> sites
+    Returns:
+        Within site confusion score
+    """
+    within_site = 0
+    cross_site = 0    
+    for index, value in enumerate(y_pred):
+        #If not correctly predicted
+        if not value == y_true[index]:
+            correct_sites = site_lists[y_true[index]]
+            
+            try:
+                incorrect_site = site_lists[y_pred[index]]
+            except Exception as e:
+                print(e)
+                continue
+        
+            #Do they co-occur?
+            site_overlap = any([site in incorrect_site for site in correct_sites])
+            if site_overlap:
+                within_site +=1
+            else:
+                cross_site +=1   
+        else:
+            pass
+    
+    #don't divide by zero
+    if within_site + cross_site == 0:
+        return 0
+    
+    #Get proportion of within site error
+    proportion_within = within_site/(within_site + cross_site)
+    
+    return proportion_within
 
+def genus_confusion(y_true, y_pred):
+    """What proportion of misidentified species come from the same genus?
+    Args: 
+        y_true: taxonID of true labels
+        y_pred: taxonID of predicted labels
+        scientific_dict: a dict of taxonID -> scientific name
+    Returns:
+        Within site confusion score
+    """
+    within_site = 0
+    cross_site = 0    
+    for index, value in enumerate(y_pred):
+        #If not correctly predicted
+        if not value == y_true[index]:
+            true_sci_name = scientific_dict[y_true[index]].split().str.get(0)
+            pred_sci_name = scientific_dict[y_pred[index]]
+
+            #Do they co-occur?
+            site_overlap = any([site in incorrect_site for site in correct_sites])
+            if site_overlap:
+                within_site +=1
+            else:
+                cross_site +=1   
+        else:
+            pass
+    
+    #don't divide by zero
+    if within_site + cross_site == 0:
+        return 0
+    
+    #Get proportion of within site error
+    proportion_within = within_site/(within_site + cross_site)
+    
+    return proportion_within
+
+        
 def f1_scores(y_true, y_pred):
     """Calculate micro, macro
     Args:
