9aff9c53ee933e9a0d396d3a45c4e44bf345e00a
bw4sz <benweinstein2010@gmail.com>
2020-11-12 19:45:08 -0800

    create plot after run

--- experiments/Trees/run.py
+++ experiments/Trees/run.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     model.ensemble_model.save("{}/Ensemble.h5".format(save_dir))
     
     #Plots
-    visualize.plot_crown_position(model = model.ensemble_model, train_shp = model.config["evaluation"]["ground_truth_path"], eval_dataset_with_index=model.val_split_with_ids)
+    visualize.plot_crown_position(model = model.ensemble_model, train_path = model.config["evaluation"]["ground_truth_path"], eval_dataset_with_index=model.val_split_with_ids)
     
     #save predictions
     
