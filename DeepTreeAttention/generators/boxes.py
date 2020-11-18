b0857738015d2a8026df3e51b88645c8b005a9a7
bw4sz <benweinstein2010@gmail.com>
2020-11-17 18:06:03 -0800

    normalize on top

--- DeepTreeAttention/generators/boxes.py
+++ DeepTreeAttention/generators/boxes.py
@@ -454,8 +454,8 @@ def tf_dataset(tfrecords,
             
     if HSI:
         HSI_dataset = dataset.map(_HSI_parse_, num_parallel_calls=cores) 
+        HSI_dataset = HSI_dataset.map(normalize, num_parallel_calls=cores)                        
         if augmentation:
-            HSI_dataset = HSI_dataset.map(normalize, num_parallel_calls=cores)                
             HSI_dataset = HSI_dataset.map(augment, num_parallel_calls=cores)                
         inputs.append(HSI_dataset)        
         
