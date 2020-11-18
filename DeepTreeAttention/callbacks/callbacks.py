2246168e527931f0a67d6ea53b83ef59d53e2fab
bw4sz <benweinstein2010@gmail.com>
2020-11-18 13:29:17 -0800

    Drop callbacks

--- DeepTreeAttention/callbacks/callbacks.py
+++ DeepTreeAttention/callbacks/callbacks.py
@@ -223,6 +223,6 @@ def create(experiment, train_data, validation_data, train_shp, validation_data_w
     if log_dir is not None:
         print("saving tensorboard logs at {}".format(log_dir))
         tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=1, profile_batch=20)
-        callback_list.append(tensorboard)        
+        callback_list = [tensorboard]        
 
     return callback_list
