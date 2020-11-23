fe3bd7bc631ed234043fd5630313c8168f83eeee
bw4sz <benweinstein2010@gmail.com>
2020-11-23 13:50:58 -0800

    prefetch 1

--- DeepTreeAttention/generators/boxes.py
+++ DeepTreeAttention/generators/boxes.py
@@ -560,6 +560,6 @@ def tf_dataset(tfrecords,
         zipped_dataset = zipped_dataset.shuffle(buffer_size=10)   
     
     zipped_dataset = zipped_dataset.batch(batch_size=batch_size)    
-    zipped_dataset = zipped_dataset.prefetch(buffer_size=AUTO)    
+    zipped_dataset = zipped_dataset.prefetch(buffer_size=1)    
     
     return zipped_dataset
