4544e23aae5cb4cadaf735e3427c9a818f04989e
bw4sz <benweinstein2010@gmail.com>
2020-11-25 10:53:07 -0800

    first glob

--- experiments/Trees/generate.py
+++ experiments/Trees/generate.py
@@ -14,17 +14,19 @@ att = AttentionModel(config="/home/b.weinstein/DeepTreeAttention/conf/tree_confi
 client = start(cpus=3, mem_size="12GB") 
 
 #Generate training data
-train_tfrecords = []
 weak_records = glob.glob(os.path.join("/orange/idtrees-collab/species_classification/confident_predictions","*.csv"))
 weak_records = [x for x in weak_records if "BART" in x]
 weak_records = weak_records[:3]
 
 print("Running records: {}".format(weak_records))
 
+rgb_pool = glob.glob(att.config["rgb_sensor_pool"])
+hyperspectral_pool = glob.glob(att.config["hyperspectral_sensor_pool"])
+
 for record in weak_records:
     #Hot fix for the regex, sergio changed the name slightly.
     record = record.replace("itc_predictions", "image")
-    sensor_path = lookup_and_convert(record, rgb_pool=att.config["rgb_sensor_pool"], hyperspectral_pool=att.config["hyperspectral_sensor_pool"], savedir=att.config["hyperspectral_tif_dir"])
+    sensor_path = lookup_and_convert(shapefile=record, rgb_pool=rgb_pool, hyperspectral_pool=hyperspectral_pool, savedir=att.config["hyperspectral_tif_dir"])
     future = client.submit(att.generate, record=record, sensor_path=sensor_path, chunk_size=500, train=True)
     train_tfrecords.append(future)
     
