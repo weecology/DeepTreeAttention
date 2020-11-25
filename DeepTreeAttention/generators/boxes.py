44c3aca8fe19707eeac395f32e0a6af75fedd166
bw4sz <benweinstein2010@gmail.com>
2020-11-24 19:06:47 -0800

    single prefetch

--- DeepTreeAttention/generators/boxes.py
+++ DeepTreeAttention/generators/boxes.py
@@ -516,6 +516,6 @@ def tf_dataset(tfrecords,
     zipped_dataset = zipped_dataset.batch(batch_size=batch_size)
     if cache:
         zipped_dataset = zipped_dataset.cache()
-    zipped_dataset = zipped_dataset.prefetch(buffer_size=AUTO)    
+    zipped_dataset = zipped_dataset.prefetch(buffer_size=1)    
     
     return zipped_dataset
