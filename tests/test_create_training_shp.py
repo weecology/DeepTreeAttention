439f959416f35789972c08b529212885c72b534f
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-01 14:34:01 -0800

    relative data dir

--- tests/test_create_training_shp.py
+++ tests/test_create_training_shp.py
@@ -1,14 +1,11 @@
 from DeepTreeAttention.generators import create_training_shp
-from DeepTreeAttention.trees import __file__ as ROOT
 import os
 import pytest
 
-ROOT = os.path.dirname(os.path.dirname(ROOT))
-
 @pytest.fixture()
 def testdata():
-    path = "{}/data/raw/test_with_uid.csv".format(ROOT)
-    field_data_path = "{}/data/raw/2020_vst_december.csv".format(ROOT)    
+    path = "data/raw/test_with_uid.csv"
+    field_data_path = "data/raw/2020_vst_december.csv"  
     shp = create_training_shp.test_split(path, field_data_path)
     
     assert not shp.empty
@@ -16,4 +13,4 @@ def testdata():
     return shp
     
 def test_train_test_split():
-    create_training_shp.train_test_split(ROOT, debug=True)
+    create_training_shp.train_test_split(".", debug=True)
