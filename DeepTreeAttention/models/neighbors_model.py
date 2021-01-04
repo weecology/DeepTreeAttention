b2ee45670a47b2bc4a0aac6ddcaa253370e04145
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-04 11:39:01 -0800

    neighbor model

---
+++ DeepTreeAttention/models/neighbors_model.py
@@ -0,0 +1,64 @@
+#Define spatial neighbor learning
+import tensorflow as tf
+
+def define(ensemble_model, k_neighbors, classes=2, freeze=False):
+    """Define a neighbor model based on a ensemble model
+    Args:
+        ensemble_model: see Hang2020_geographic.ensemble_model
+        k_neighbors: number of neighbors in the array to use an input shape
+        freeze: whether to freeze ensemble model layers and just train new top layer softmax
+    Returns:
+        model: a tf keras model for inference
+    """
+    #Neighbor input    
+    #shape is equal to the concat shape of the ensemble model
+    if freeze:
+        for x in ensemble_model.layers:
+            x.trainable=False
+            
+    n_features = ensemble_model.get_layer("submodel_concat").output.shape[1]
+    input_shape = (k_neighbors, n_features)
+    neighbor_inputs = tf.keras.layers.Input(shape=input_shape, name="neighbor_input")
+    flatten_neighbors = tf.keras.layers.Flatten(name="flatten_inputs")(neighbor_inputs)
+    neighbor_features = tf.keras.layers.Dense(n_features, activation="relu",name="neighbor_feature_dense")(flatten_neighbors)
+    neighbor_features = tf.keras.backend.l2_normalize(neighbor_features)
+        
+    #strip off previous head layers, target features are the HSI + metadata from the target tree
+    target_features = ensemble_model.get_layer("submodel_concat").output
+    target_features = tf.keras.layers.Dense(n_features, activation="relu",name="target_feature_dense")(target_features)
+    target_features = tf.keras.backend.l2_normalize(target_features)  
+    
+    #Multiply to neighbor features
+    joined_features = tf.keras.layers.Multiply(name="target_neighbor_multiply")([target_features, neighbor_features])
+    joined_features = tf.keras.layers.Softmax()(joined_features)
+    
+    #Skip connection for neighbor features
+    neighbor_features = tf.keras.layers.Dense(n_features, activation="relu",name="skip_neighbor_feature_dense")(flatten_neighbors)
+    joined_features = tf.keras.layers.Multiply()([joined_features, neighbor_features])
+    output = tf.keras.layers.Dense(classes, name="neighbor_softmax", activation="softmax")(joined_features)
+            
+    return ensemble_model.inputs, neighbor_inputs, output
+
+def create(ensemble_model, k_neighbors, classes, freeze=False, learning_rate=0.001):
+    """Create a neighbor model based on a ensemble model
+    Args:
+        ensemble_model: see Hang2020_geographic.ensemble_model
+        k_neighbors: number of neighbors in the array to use an input shape
+        freeze: whether to freeze ensemble model layers and just train new top layer softmax
+    Returns:
+        model: a tf keras model for inference
+    """
+    
+    ensemble_model_inputs, neighbor_inputs, output = define(ensemble_model=ensemble_model, k_neighbors=k_neighbors, freeze=freeze, classes=classes)
+            
+    neighbor_model = tf.keras.Model([ensemble_model_inputs, neighbor_inputs], output)
+    
+    metric_list = [tf.keras.metrics.CategoricalAccuracy(name="acc")]    
+    
+    neighbor_model.compile(
+        loss="categorical_crossentropy",
+        optimizer=tf.keras.optimizers.Adam(
+            lr=float(learning_rate)),
+        metrics=metric_list)    
+    
+    return neighbor_model
--- tests/test_neighbors_model.py
+++ tests/test_neighbors_model.py
@@ -1,6 +1,7 @@
 #test create model
 from DeepTreeAttention.models import Hang2020_geographic as Hang
 from DeepTreeAttention.models import metadata
+from DeepTreeAttention.models import neighbors_model
 import pytest
 import numpy as np
 import tensorflow as tf
@@ -21,33 +22,28 @@ def metadata_data():
     return [elevation, sites, domains]
 
 @pytest.fixture()
-def RGB_image():
-    image = np.zeros((1, 200, 200, 3), dtype=tf.keras.backend.floatx())
-    return image
-
-@pytest.fixture()
 def HSI_image():
-    image = np.zeros((1, 20, 20, 369), dtype=tf.keras.backend.floatx())
+    image = np.zeros((1,20, 20, 369), dtype=tf.keras.backend.floatx())
     return image
 
-#Test full model makes the correct number of predictions.
-@pytest.mark.parametrize("classes", [3, 10, 20])
-def test_model(RGB_image, classes):
-    batch, height, width, channels = RGB_image.shape 
-    sensor_inputs, sensor_outputs, spatial, spectral = Hang.define_model(classes=classes, height=height, width=width, channels=channels)    
-    model = tf.keras.Model(inputs=sensor_inputs, outputs=sensor_outputs)
-    prediction = model.predict(RGB_image)
-    assert prediction.shape == (1, classes)
-
-def test_ensemble(HSI_image, metadata_data):    
+def test_neighbors(HSI_image, metadata_data):
     batch, height, width, channels = HSI_image.shape     
     sensor_inputs, sensor_outputs, spatial, spectral = Hang.define_model(classes=2, height=height, width=width, channels=channels)    
     model1 = tf.keras.Model(inputs=sensor_inputs, outputs=sensor_outputs)
     
     metadata_model = metadata.create(classes=2, sites=10, domains =10, learning_rate=0.001)
     ensemble = Hang.learned_ensemble(HSI_model=model1, metadata_model=metadata_model, classes=2)
-    prediction = ensemble.predict([HSI_image] + metadata_data)
-    assert prediction.shape == (1, 2)
+    ensemble = tf.keras.Model(ensemble.inputs, ensemble.get_layer("submodel_concat").output)
     
+    neighbor_array = []
+    for x in np.arange(5):
+        prediction = ensemble.predict([HSI_image] + metadata_data)
+        neighbor_array.append(prediction)
     
+    #stack and batch
+    neighbor_array = np.vstack(neighbor_array)
+    neighbor_array = np.expand_dims(neighbor_array, axis=0)
     
+    neighbor_model = neighbors_model.create(ensemble_model = ensemble, freeze=True, k_neighbors=5, classes=2)
+    prediction = neighbor_model.predict([HSI_image] + metadata_data + [neighbor_array])
+    assert prediction.shape == (1,2)
