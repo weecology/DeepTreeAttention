62b8acad5e9cba56a29a35190fb0a4916af414bc
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-15 19:11:40 -0800

    negative mask

--- DeepTreeAttention/models/neighbors_model.py
+++ DeepTreeAttention/models/neighbors_model.py
@@ -47,7 +47,7 @@ def define(ensemble_model, k_neighbors, classes=2, freeze=False):
     joined_features = tf.keras.layers.Lambda(lambda x: x/(0.1 *10.58))(joined_features)
     
     #Zero out any masked entries
-    joined_features = tf.where(joined_features=0, joined_features, tf.float64.min)
+    joined_features = tf.where(joined_features!=0, joined_features, -999)
     joined_features = tf.keras.layers.Softmax(name="Attention_softmax")(joined_features)
     
     #Skip connection for value features
