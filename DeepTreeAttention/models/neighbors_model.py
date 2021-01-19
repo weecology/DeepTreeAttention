9c2e1a080cb2aa9f471133f650df74bf07492108
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-19 10:54:45 -0800

    only one fc layer

--- DeepTreeAttention/models/neighbors_model.py
+++ DeepTreeAttention/models/neighbors_model.py
@@ -59,7 +59,6 @@ def define(ensemble_model, k_neighbors, classes=2, freeze=False):
     context_residual = WeightedSum(name="ensemble_add_bias")([context_vector,original_features])
     
     merged_layers = tf.keras.layers.Dropout(0.8)(context_residual)
-    merged_layers = tf.keras.layers.Dense(classes,name="ensemble_learn_bottleneck",activation="relu")(merged_layers)
     output = tf.keras.layers.Dense(classes,name="ensemble_learn",activation="softmax")(merged_layers)
     
     return ensemble_model.inputs, neighbor_inputs, neighbor_distances, output
