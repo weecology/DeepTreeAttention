8b08a01a72ff99bd59af2954fd35643802b93eef
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-24 19:28:34 -0800

    wrong config file

--- experiments/Trees/extract_examples.py
+++ experiments/Trees/extract_examples.py
@@ -4,5 +4,5 @@
 from DeepTreeAttention.visualization import extract
 from DeepTreeAttention.trees import AttentionModel
 
-mod = AttentionModel()
+mod = AttentionModel(config="/home/b.weinstein/DeepTreeAttention/conf/tree_config.yml")
 extract.save_images_to_matlab(mod, classes=["ABLAL","PIEN"], savedir="/orange/idtrees-collab/DeepTreeAttention/examples/")
