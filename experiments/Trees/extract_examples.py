28ff85643395b38a68d731b99203e0e9e20eca65
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-01-24 19:34:20 -0800

    another pair

--- experiments/Trees/extract_examples.py
+++ experiments/Trees/extract_examples.py
@@ -5,4 +5,4 @@ from DeepTreeAttention.visualization import extract
 from DeepTreeAttention.trees import AttentionModel
 
 mod = AttentionModel(config="/home/b.weinstein/DeepTreeAttention/conf/tree_config.yml")
-extract.save_images_to_matlab(mod, classes=["ABLAL","PIEN"], savedir="/orange/idtrees-collab/DeepTreeAttention/examples/")
+extract.save_images_to_matlab(mod, classes=["ACRU","QURU"], savedir="/orange/idtrees-collab/DeepTreeAttention/examples/")
