96ee0d91a609fde13772a05243c226243a949324
bw4sz <benweinstein2010@gmail.com>
2021-02-12 14:53:51 -0500

    merge

--- experiments/Trees/generate.py
+++ experiments/Trees/generate.py
@@ -82,7 +82,7 @@ def run(record, savedir, raw_box_dir):
         elevation=elevation,
         label_column="taxonID",
         species_label_dict=species_label_dict,
-        ensemble_model=ensemble_model,
+        ensemble_model=None,
         savedir=savedir
     )
     
