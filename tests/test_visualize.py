9550f0135b760ca1f6fe7b939a3cbe46e51ea352
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-28 14:42:18 -0700

    Clean main branch to create env

--- tests/test_visualize.py
+++
@@ -1,9 +0,0 @@
-#test visualize
-from DeepTreeAttention.visualization import visualize
-
-def test_error_crown_position():
-    y_true = [0,1,2,2,2]
-    y_pred = [1,2,2,2,2]
-    box_index = [0,2,1,3,4]
-    canopydict = {0:"b",1:"a", 2:"b",3:"b",4:"a"}
-    visualize.canopyPosition_barplot(y_true, y_pred, box_index, canopydict)
