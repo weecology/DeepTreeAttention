32e57f6d5c42a25281337aa4dc9ae2de2b62e253
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-30 11:45:11 -0700

    spectral subnetwork

--- src/models/Hang2020.py
+++ src/models/Hang2020.py
@@ -1,14 +1,21 @@
-#Hang et al. 2020
+#Hang et al. 2020 https://arxiv.org/pdf/2005.11977.pdf
 from torch.nn import Module
 from torch.nn import functional as F
 from torch import nn
 import torch
 
+def global_spectral_pool(x):
+    """Helper function to keep the same dimensions after pooling to avoid resizing each time"""
+    global_pool = torch.mean(x,dim=(2,3))
+    global_pool = global_pool.unsqueeze(-1)
+    
+    return global_pool
+    
 class conv_module(Module):
     def __init__(self, in_channels, filters, maxpool_kernel=None):
         """Define a simple conv block with batchnorm and optional max pooling"""
         super(conv_module, self).__init__()
-        self.conv_layer = nn.Conv2d(in_channels, out_channels=filters, kernel_size = (3,3)) 
+        self.conv_layer = nn.Conv2d(in_channels, out_channels=filters, kernel_size = (3,3), padding="same") 
         self.bn1 = nn.BatchNorm2d(filters)                    
         self.maxpool_kernal = maxpool_kernel
         if maxpool_kernel:
@@ -23,7 +30,7 @@ class conv_module(Module):
 
 class spatial_attention(Module):
     """
-    Learn cross band spectral features with a set of convolutions and spectral pooling attention layers
+    Learn cross band spatial features with a set of convolutions and spectral pooling attention layers
     """
     def __init__(self, filters, classes):
         super(spatial_attention,self).__init__()
@@ -40,7 +47,6 @@ class spatial_attention(Module):
             raise ValueError(
                 "Unknown incoming kernel size {} for attention layers".format(kernel_size))
         
-        #TOOD check padding
         self.attention_conv1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding="same")
         self.attention_conv2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding="same")
         
@@ -74,7 +80,58 @@ class spatial_attention(Module):
         class_probabilities = F.softmax(class_features)
         
         return class_probabilities
+
+class spectral_attention(Module):
+    """
+    Learn cross band spectral features with a set of convolutions and spectral pooling attention layers
+    The feature maps should be pooled to remove spatial dimensions before reading in the module
+    Args:
+        in_channels: number of feature maps of the current image
+    """
+    def __init__(self, filters, classes):
+        super(spectral_attention, self).__init__()        
+        # Weak Attention with adaptive kernel size based on size of incoming feature map
+        if filters == 32:
+            kernel_size = 3
+            in_features = 640
+        elif filters == 64:
+            kernel_size = 5
+            in_features = 1280
+        elif filters == 128:
+            in_features = 2560
+            kernel_size = 7
+        else:
+            raise ValueError(
+                "Unknown incoming kernel size {} for attention layers".format(kernel_size))
         
+        self.attention_conv1 = nn.Conv1d(in_channels=filters, out_channels=filters, kernel_size=kernel_size, padding="same")
+        self.attention_conv2 = nn.Conv1d(in_channels=filters, out_channels=filters, kernel_size=kernel_size, padding="same")
+        
+        #Does this pool size change base on in_features?
+        self.fc1 = nn.Linear(in_features=in_features, out_features=classes)
+        
+    def forward(self, x):
+        """Calculate attention and class scores for batch"""
+        #Global pooling and add dimensions to keep the same shape
+        pooled_features = global_spectral_pool(x)
+        
+        #Attention layers
+        attention = self.attention_conv1(pooled_features)
+        attention = F.relu(attention)
+        attention = self.attention_conv2(attention)
+        attention = F.sigmoid(attention)
+        #Add dummy dimension to make the shapes the same
+        attention = attention.unsqueeze(-1)
+        attention = torch.mul(x, attention)
+        
+        # Classification Head
+        pooled_attention_features = global_spectral_pool(attention)
+        pooled_attention_features = torch.flatten(pooled_attention_features)
+        class_features = self.fc1(pooled_attention_features)
+        class_scores = F.softmax(class_features)
+        
+        return attention, class_scores
+    
 class spatial_network(Module):
     """
         Learn spatial features with alternating convolutional and attention pooling layers
@@ -86,24 +143,58 @@ class spatial_network(Module):
         self.conv1 = conv_module(in_channels=bands, filters=32)
         self.attention_1 = spatial_attention(filters=32, classes = classes)
     
-    def forward(self, x):
-        features = self.conv1(x)
-        features = self.attention_1(features)
-        
-        return features
+        self.conv2 = conv_module(in_channels=bands, filters=64, maxpool_kernel=(2,2))
+        self.attention_2 = spatial_attention(filters=64, classes = classes)
     
-def spectral_network(classes):
-    pass
-
+        self.conv3 = conv_module(in_channels=bands, filters=128, maxpool_kernel=(2,2))
+        self.attention_3 = spatial_attention(filters=128, classes = classes)
+    
+    def forward(self, x):
+        """The forward method is written for training the joint scores of the three attention layers"""
+        x = self.conv1(x)
+        x, scores1 = self.attention_1(x)
+        x = self.conv2(x)
+        x, scores2 = self.attention_2(x)
+        x = self.conv(x)        
+        x, scores3 = self.attention_3(x)
+        
+        return [scores1,scores2,scores3]
+    
+class spectral_network(Module):
+    """
+        Learn spectral features with alternating convolutional and attention pooling layers
+    """
+    def __init__(self, bands, classes):
+        super(spectral_network, self).__init__()
+        
+        #First submodel is 32 filters
+        self.conv1 = conv_module(in_channels=bands, filters=32)
+        self.attention_1 = spectral_attention(filters=32, classes = classes)
+    
+        self.conv2 = conv_module(in_channels=32, filters=64, maxpool_kernel=(2,2))
+        self.attention_2 = spectral_attention(filters=64, classes = classes)
+    
+        self.conv3 = conv_module(in_channels=64, filters=128, maxpool_kernel=(2,2))
+        self.attention_3 = spectral_attention(filters=128, classes = classes)
+    
+    def forward(self, x):
+        """The forward method is written for training the joint scores of the three attention layers"""
+        x = self.conv1(x)
+        x, scores1 = self.attention_1(x)
+        x = self.conv2(x)
+        x, scores2 = self.attention_2(x)
+        x = self.conv3(x)        
+        x, scores3 = self.attention_3(x)
+        
+        return [scores1,scores2,scores3]
+        
 def subnetwork_consensus(classes):
     pass
 
-class Hang2020(Module):
+class SpatialModel(Module):
     def __init__(self, bands, classes):
-        super(Hang2020, self).__init__()
+        super(SpatialModel, self).__init__()
         self.spatial_attention = spatial_network(bands=bands, classes=classes)
-        #self.spectral_attention = spectral_network(classes, bands)
-        #self.consensus_layer = subnetwork_consensus(classes)
         self.fc1 = nn.Linear(in_features=10, out_features=classes)
         
     def forward(self, x):
--- tests/test_Hang2020.py
+++ tests/test_Hang2020.py
@@ -2,16 +2,35 @@
 from src.models import Hang2020
 import torch
 import os
+import pytest
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-
-#This test assume a crop size of 11 x 11 x 369.
-
+def test_conv_module():
+    m = Hang2020.conv_module(in_channels=369, filters=32)
+    image = torch.randn(20, 369, 11, 11)
+    output = m(image)
+    assert output.shape == (20,32,11,11)
+    
 def test_spatial_attention():
     m = Hang2020.spatial_attention(filters=32, classes=10)
     image = torch.randn(20, 32, 9, 9)
     output = m(image)
     assert output.shape[0] == 10
+    
+@pytest.mark.parametrize("conv_dimension",[(20,32,11,11),(20,64,5,5),(20,128,2,2)])
+def test_spectral_attention(conv_dimension):
+    """Check spectral attention for each convoutional dimension"""
+    m = Hang2020.spectral_attention(filters=conv_dimension[1], classes=10)
+    image = torch.randn(conv_dimension)
+    attention, scores = m(image)
+    assert scores.shape[0] == 10
+    
+def test_spectral_network():
+    m = Hang2020.spectral_network(bands=369, classes=10)
+    image = torch.randn(20, 369, 11, 11)
+    output = m(image)
+    assert len(output) == 3
+    assert output[0].shape[0] == 10
     
 def test_Hang2020():
     m = Hang2020.Hang2020(bands=369, classes=10)
