ebb216a1aeca54556a75759005d6bd4419903b48
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-08-17 12:19:18 -0700

    typo in test patches

--- tests/test_patches.py
+++ tests/test_patches.py
@@ -8,6 +8,6 @@ ROOT = os.path.dirname(os.path.dirname(ROOT))
 
 def test_patches():
     gdf = gpd.read_file("{}/tests/data/crown.shp".format(ROOT))
-    patch_list = patches.crown_to_pixel(crown=gdf.geometry[0],rgb_path="{}/tests/data/2019_D01_HARV_DP3_726000_4699000_image_crop.tif".format(ROOT))
+    patch_list = patches.crown_to_pixel(crown=gdf.geometry[0],img_path="{}/tests/data/2019_D01_HARV_DP3_726000_4699000_image_crop.tif".format(ROOT))
     assert len(patch_list) > 0 
     assert patch_list[0].shape == (3,11,11)
