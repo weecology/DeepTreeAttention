3e1d6fe9c2ed9227453e0f8df47e47ecd9f7bda7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-08-23 15:06:04 -0700

    bounded window read

--- src/patches.py
+++ src/patches.py
@@ -11,8 +11,8 @@ def row_col_from_crown(crown, src):
     img_centroids = []
     img = src.read(window=rasterio.windows.from_bounds(left, bottom, right, top, transform=src.transform))
     win_transform = src.window_transform(window=rasterio.windows.from_bounds(left, bottom, right, top, transform=src.transform))
-    for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
+    for row in range(img.shape[1]):
+        for col in range(img.shape[2]):
             coords = rasterio.transform.xy(win_transform, row, col, offset='center')
             row_col = rasterio.transform.rowcol(src.transform, coords[0], coords[1])
             img_centroids.append(row_col)    
@@ -27,7 +27,7 @@ def crown_to_pixel(crown, img_path, savedir, basename,width=11, height=11):
     img_centroids = row_col_from_crown(crown, src)
     for indices in img_centroids:
         row, col = indices
-        img = src.read(window = rasterio.windows.Window(col_off=col, row_off=row, width = width, height=height), boundless=True)
+        img = src.read(window = rasterio.windows.Window(col_off=col, row_off=row, width = width, height=height), boundless=False)
         filename = "{}/{}_{}.tif".format(savedir, basename, counter)
         with rasterio.open(filename, "w", driver="GTiff",height=height, width=width, count = img.shape[0], dtype=img.dtype) as dst:
             dst.write(img)
