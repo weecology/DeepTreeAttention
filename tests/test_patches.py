52a3af76761a0e78179f337c0977b64cdd6182db
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-08-23 14:23:44 -0700

    add hsi data test

--- tests/test_patches.py
+++ tests/test_patches.py
@@ -3,11 +3,20 @@ import os
 from src import patches
 from src import __file__ as ROOT
 import geopandas as gpd
+import rasterio
 
 ROOT = os.path.dirname(os.path.dirname(ROOT))
 
-def test_patches():
+def test_patches_rgb(tmpdir):
     gdf = gpd.read_file("{}/tests/data/crown.shp".format(ROOT))
-    patch_list = patches.crown_to_pixel(crown=gdf.geometry[0],img_path="{}/tests/data/2019_D01_HARV_DP3_726000_4699000_image_crop.tif".format(ROOT))
+    patch_list = patches.crown_to_pixel(crown=gdf.geometry[0],img_path="{}/tests/data/2019_D01_HARV_DP3_726000_4699000_image_crop.tif".format(ROOT), savedir=tmpdir, basename="test")
     assert len(patch_list) > 0 
-    assert patch_list[0].shape == (3,11,11)
+    img = rasterio.open(patch_list[0]).read()
+    assert img.shape == (3,11,11)
+    
+def test_patches_hsi(tmpdir):
+    gdf = gpd.read_file("{}/tests/data/crown.shp".format(ROOT))
+    patch_list = patches.crown_to_pixel(crown=gdf.geometry[0],img_path="{}/tests/data/hsi/2019_HARV_6_726000_4699000_image_crop_hyperspectral.tif".format(ROOT), savedir=tmpdir, basename="test")
+    assert len(patch_list) > 0 
+    img = rasterio.open(patch_list[0]).read()
+    assert img.shape == (369,11,11)    
