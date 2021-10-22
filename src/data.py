bbdf57cfc52c42f66cf7e9b9d93509ae3df78ec8
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-10-22 14:47:15 -0700

    maximize species

--- src/data.py
+++ src/data.py
@@ -136,6 +136,7 @@ def train_test_split(shp, config, client = None):
     print("splitting data into train test. Initial data has {} points from {} species".format(shp.shape[0],shp.taxonID.nunique()))
     np.random.seed(1)
     test_species = 0
+    ties = []
     if client:
         futures = [ ]
         for x in np.arange(config["iterations"]):
@@ -144,7 +145,7 @@ def train_test_split(shp, config, client = None):
         
         for x in as_completed(futures):
             train, test = x.result()
-            if test.shape[0] > test_species:
+            if test.taxonID.nunique() > test_species:
                 print("Selected test has {} points and {} species".format(test.shape[0], test.taxonID.nunique()))
                 saved_train = train
                 saved_test = test
@@ -152,11 +153,19 @@ def train_test_split(shp, config, client = None):
     else:
         for x in np.arange(config["iterations"]):
             train, test = sample_plots(shp, min_train_samples=config["min_train_samples"], min_test_samples=config["min_test_samples"])
-            if test.shape[0] > test_species:
+            if test.taxonID.nunique() > test_species:
                 print("Selected test has {} points and {} species".format(test.shape[0], test.taxonID.nunique()))
                 saved_train = train
                 saved_test = test
                 test_species = test.taxonID.nunique()
+                #reset ties
+                ties = []
+            elif test.taxonID.nunique() == test_species:
+                print("ties")
+                ties.append(test)
+    
+    # The size of the datasets
+    print("The size of tied datasets with {} species is {}".format([test_species, x.shape[0] for x in ties]))
     
     train = saved_train
     test = saved_test    
