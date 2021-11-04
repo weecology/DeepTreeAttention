45d3d6c005844e288f86b597504d6703796de0e9
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-11-04 14:16:39 -0700

    joint loss

--- src/models/simulation.py
+++ src/models/simulation.py
@@ -96,9 +96,9 @@ class autoencoder(LightningModule):
         
         autoencoder_loss = F.mse_loss(autoencoder_yhat, images)    
         classification_loss = F.cross_entropy(classification_yhat, observed_labels)
-        #loss = autoencoder_loss + (classification_loss * 0.1)
+        loss = autoencoder_loss + (classification_loss * 0.1)
 
-        return classification_loss
+        return loss
 
     def validation_step(self, batch, batch_idx):
         """Train on a loaded dataset
@@ -115,13 +115,13 @@ class autoencoder(LightningModule):
         
         classification_loss = F.cross_entropy(classification_yhat, observed_labels, ignore_index=-1)
         
-        #loss = autoencoder_loss + (classification_loss * 0.1)
+        loss = autoencoder_loss + (classification_loss * 0.1)
         
         softmax_prob = F.softmax(classification_yhat, dim=1)
         softmax_prob = F.pad(input=softmax_prob, pad=(0, 2, 0, 0), mode='constant', value=0)
         
         output = self.metrics(softmax_prob, true_labels) 
-        self.log("val_loss", classification_loss, on_epoch=True)
+        self.log("val_loss", loss, on_epoch=True)
         self.log_dict(output, on_epoch=True, on_step=False)
         
         return classification_loss
