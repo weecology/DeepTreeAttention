ede63873b3a7e9493b8021012c32a8e3b24918c7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-11-11 15:51:29 -0800

    hist

--- src/outlier.py
+++ src/outlier.py
@@ -119,7 +119,10 @@ def distance_outliers(results, features, labels, threshold, experiment):
         
         #Plot centroid distance
         for x in centroids:
-            cluster_points = results[results.observed_label == x]
+            cluster_points = results[results.observed_label == x]            
+            cluster_points["centroid_distance"].hist()
+            experiment.log_figure("class {} centroid distance")
+            
             centroid_plot = visualize.plot_2d_layer(features[results.observed_label == x,:], labels=cluster_points["distance_outlier"].astype(int), size_weights=cluster_points["distance_outlier"].astype(int)+1)
             plt.plot(centroids[x][0], centroids[x][1],'go')
             plt.title("Class {} predicted outliers".format(x))
