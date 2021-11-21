d47bab27cf33ea4de56462f3243d960e36d21cb7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-11-21 10:03:19 -0800

    Same conditions as main

--- src/main.py
+++ src/main.py
@@ -87,10 +87,9 @@ class TreeModel(LightningModule):
                                                          factor=0.5,
                                                          patience=10,
                                                          verbose=True,
-                                                         threshold=0.001,
+                                                         threshold=0.0001,
                                                          threshold_mode='rel',
                                                          cooldown=0,
-                                                         min_lr=0.00001,
                                                          eps=1e-08)
         
         return {'optimizer':optimizer, 'lr_scheduler': scheduler,"monitor":'val_loss'}
