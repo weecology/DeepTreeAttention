8a6d3b63fc10a5ef97d7507fec7dee810fec89d6
bw4sz <benweinstein2010@gmail.com>
2021-11-24 13:12:48 -0500

    merge

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -8,8 +8,8 @@ sbatch <<EOT
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=70GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepTreeAttention_%j.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/DeepTreeAttention_%j.err
