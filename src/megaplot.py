4443fedc9244f3c2c2d6cdeaa54460361ccc9d84
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-11-30 11:45:17 -0800

    change megaplot naming

--- src/megaplot.py
+++ src/megaplot.py
@@ -36,7 +36,7 @@ def format(site, gdf, directory, config):
     gdf = gdf.merge(species_data[["sp","taxonID"]])
     
     #give each an individual ID
-    gdf["individualID"] = gdf.index.to_series().apply(lambda x: "{}_contrib{}".format(site,x)) 
+    gdf["individualID"] = gdf.index.to_series().apply(lambda x: "{}.contrib.{}".format(site,x)) 
     gdf["siteID"] = site
     
     #PlotID variable to center on correct tile
