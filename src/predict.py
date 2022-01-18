0d45a67deb3c00d8137f161cb14218f8a50174fe
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-01-18 13:22:11 -0800

    as rasterio on the fly loader

--- src/predict.py
+++ src/predict.py
@@ -3,10 +3,9 @@ from deepforest import main
 from deepforest.utilities import annotations_to_shapefile
 import glob
 import rasterio
-from src import generate
 from src.main import TreeModel
 from src import data 
-from src import start_cluster
+from torch.utils.data import Dataset
 from src import neon_paths
 import os
 from torchvision import transforms
@@ -14,6 +13,38 @@ from torch.nn import functional as F
 import torch
 import numpy as np
 
+class on_the_fly_Dataset(Dataset):
+    """A csv file with a path to image crop and label
+    Args:
+       crowns: geodataframe of crown locations from a single rasterio src
+       image_path: .tif file location
+    """
+    def __init__(self, crowns, image_path, config=None):
+        self.config = config 
+        self.crowns = crowns
+        self.image_size = config["image_size"]
+        self.src = rasterio.open(image_path)
+        
+    def __len__(self):
+        #0th based index
+        return self.crowns.shape[0]
+        
+    def __getitem__(self, index):
+        geom = self.crowns.iloc[index].geometry
+        individual = self.crowns.iloc[index].individual
+        left, bottom, right, top = geom.bounds
+        crop = self.src.read(window=rasterio.windows.from_bounds(left, bottom, right, top, transform=self.src.transform)) 
+        
+        #preprocess and batch
+        image = data.preprocess_image(crop, channel_is_first=True)
+        image = transforms.functional.resize(image, size=(self.config["image_size"],self.config["image_size"]), interpolation=transforms.InterpolationMode.NEAREST)
+        
+        inputs = {}
+        inputs["HSI"] = image
+    
+        return individual, inputs
+        
+            
 def predict_tile(PATH, model_path, config, min_score, taxonIDs, client = None):
     #get rgb from HSI path
     HSI_basename = os.path.basename(PATH)
@@ -53,57 +84,19 @@ def predict_crowns(PATH):
     
     return gdf
 
-def create_crops(crowns, config, client = None):
-    
-    crops = generate.generate_crops(gdf=crowns,
-                                    sensor_glob=config["HSI_sensor_pool"],
-                                    rgb_glob = config["rgb_sensor_pool"],
-                                    convert_h5=config["convert_h5"],
-                                    HSI_tif_dir=config["HSI_tif_dir"],
-                                    savedir=config["crop_dir"],
-                                    client=client)
-    crops["individual"] = crops["individualID"]
-    crops = crops.merge(crowns[["individual","geometry"]], on="individual")
-    
-    return crops
-
 def predict_species(crowns, HSI_path, model_path, config):
     m = TreeModel.load_from_checkpoint(model_path)
-    src = rasterio.open(HSI_path) 
-    preds = []
-    scores = []
-    for geom in crowns.geometry:
-        left, bottom, right, top = geom.bounds
-        crop = src.read(window=rasterio.windows.from_bounds(left, bottom, right, top, transform=src.transform)) 
-        if crop.size == 0:
-            preds.append(None)
-            scores.append(None)
-            continue
-            
-        #preprocess and batch
-        image = data.preprocess_image(crop, channel_is_first=True)
-        image = transforms.functional.resize(image, size=(config["image_size"],config["image_size"]), interpolation=transforms.InterpolationMode.NEAREST)
-        image = torch.unsqueeze(image, dim = 0)
-        
-        #Classify pixel crops
-        m.model.eval() 
-        with torch.no_grad():
-            class_probs = m.model(image) 
-            class_probs = F.softmax(class_probs, 1)
-        class_probs = class_probs.detach().numpy()
-        index = np.argmax(class_probs)
-        label = m.index_to_label[index]
-        
-        #Average score for selected label
-        score = class_probs[:,index]
-        
-        preds.append(label)
-        scores.append(score)
+    ds = on_the_fly_Dataset(crowns, HSI_path, config)
+    data_loader = torch.utils.data.DataLoader(
+        ds,
+        batch_size=config["batch_size"],
+        shuffle=False,
+        num_workers=config["workers"],
+    )
+    df = m.predict_dataloader(data_loader, train=False)
+    df = df.merge(crowns[["individual","geometry"]], on="individual")
     
-    crowns["pred_taxa_top1"] = preds
-    crowns["top1_score"] = scores
-
-    return crowns
+    return df
 
 def choose_trees(trees, min_score, taxonIDs):
     trees = trees[trees.top1_score > min_score]
