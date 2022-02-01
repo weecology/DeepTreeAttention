f801112ad22b390ffce4fb0f61c650915c92f783
Ethan White <ethan@weecology.org>
2022-02-01 13:42:12 -0500

    Add minimum Python requirement
    Based on 41c3d225d61a6245e6fd522e34967c2a511c385e we have issues
    with older Python versions.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
+    python_requires='>=3.9'
     description='Tree Species Prediction for the National Ecological Observatory Network (NEON)',
     author='Ben Weinstein',
     license='MIT',
