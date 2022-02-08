905494e1f8942f393ac7155ec5b5523d8a002fb2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-02-07 17:17:28 -0800

    lr

--- src/models/spatial.py
+++ src/models/spatial.py
@@ -128,7 +128,6 @@ class spatial_fusion(LightningModule):
                                                          threshold=0.0001,
                                                          threshold_mode='rel',
                                                          cooldown=0,
-                                                         min_lr=0.000001,
                                                          eps=1e-08)
                                                                  
         return {'optimizer':optimizer, 'lr_scheduler': scheduler,"monitor":'val_loss'}
