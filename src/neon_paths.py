6b4b45a6c967af1ed177651cca0e2b4582e99606
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-02-17 12:10:02 -0800

    replace geoindex

--- src/neon_paths.py
+++ src/neon_paths.py
@@ -70,7 +70,7 @@ def convert_h5(hyperspectral_h5_path, rgb_path, savedir, year=None):
     return tif_path
 
 
-def lookup_and_convert(rgb_pool, hyperspectral_pool, savedir, bounds = None, geoindex=None, shapefile=None, multi_year=False):
+def lookup_and_convert(rgb_pool, hyperspectral_pool, savedir, bounds = None, geo_index=None, shapefile=None, multi_year=False):
     """For a given geo_index bounds, lookup the h5 tile and convert to .tif"""
     hyperspectral_h5_path = find_sensor_path(shapefile=shapefile,lookup_pool=hyperspectral_pool, bounds=bounds, geoindex=geoindex, multi_year=multi_year)
     rgb_path = find_sensor_path(shapefile=shapefile, lookup_pool=rgb_pool, bounds=bounds, geoindex=geoindex)
