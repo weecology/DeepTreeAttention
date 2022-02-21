1e6e842c8c1b0ef0af71fd81257d0e5c0448c5d2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-02-21 13:01:11 -0800

    OSBS only test

--- src/data.py
+++ src/data.py
@@ -118,6 +118,8 @@ def sample_plots(shp, min_train_samples=5, min_test_samples=3, iteration = 1):
         train = shp[shp.plotID == shp.plotID.unique()[1]]
 
         return train, test
+    else:
+        plotIDs = shp[shp.siteID=="OSBS"].plotID.unique()
 
     np.random.shuffle(plotIDs)
     species_to_sample = shp.taxonID.unique()
