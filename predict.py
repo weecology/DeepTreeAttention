66ada944dca656d4318843cee5742108940ab688
bw4sz <benweinstein2010@gmail.com>
2022-02-23 18:06:23 -0500

    full predict

--- predict.py
+++ predict.py
@@ -38,7 +38,7 @@ tiles = find_rgb_files(site="OSBS", config=config, year="2019")
 hyperspectral_pool = glob(config["HSI_sensor_pool"], recursive=True)
 rgb_pool = glob(config["rgb_sensor_pool"], recursive=True)
 
-cpu_client = start(cpus=1)
+cpu_client = start(cpus=10)
 
 tif_futures = cpu_client.map(convert, tiles, hyperspectral_pool=hyperspectral_pool, savedir = config["HSI_tif_dir"], year="2019")
 wait(tif_futures)
@@ -53,12 +53,12 @@ for x in tif_futures:
         pass
 
 cpu_client.close()    
-gpu_client = start(gpus=1, mem_size="50GB")
+gpu_client = start(gpus=10, mem_size="50GB")
 
 #No daemonic dask children
 config["workers"] = 0
 futures =  []
-for x in hsi_tifs[:1]:
+for x in hsi_tifs:
     future = gpu_client.submit(predict.predict_tile, x, dead_model_path = dead_model_path, species_model_path=species_model_path, config=config)
     futures.append(future)
 
