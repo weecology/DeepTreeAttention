4f7ea58d881020162adde66e030ba34f39324b0d
bw4sz <benweinstein2010@gmail.com>
2022-03-04 14:38:11 -0500

    new split

--- predict.py
+++ predict.py
@@ -43,7 +43,7 @@ cpu_client = start(cpus=10)
 tif_futures = cpu_client.map(convert, tiles, hyperspectral_pool=hyperspectral_pool, savedir = config["HSI_tif_dir"], year="2019")
 wait(tif_futures)
 
-species_model_path = "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/107a9f3164de44bda34d1c10529b5240.pl"
+species_model_path = "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/ea66ce752536451492e05ca52c4381dd.pl"
 dead_model_path = "/orange/idtrees-collab/DeepTreeAttention/Dead/snapshots/c4945ae57f4145948531a0059ebd023c.pl"
 hsi_tifs = []
 for x in tif_futures:
