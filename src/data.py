52650b777448ee7ea4c931042815e77d05cee914
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-03-06 19:02:48 -0800

    natural sampling

--- src/data.py
+++ src/data.py
@@ -535,28 +535,12 @@ class TreeData(LightningDataModule):
             )            
 
     def train_dataloader(self):
-        """Load a training file. The default location is saved during self.setup(), to override this location, set self.train_file before training"""               
-        #get class weights
-        train = pd.read_csv(self.train_file)
-        class_weights = train.label.value_counts().to_dict()     
-        data_weights = []
-        
-        #balance classes
-        for idx in range(len(self.train_ds)):
-            path, image, targets = self.train_ds[idx]
-            label = int(targets.numpy())
-            class_freq = class_weights[label]
-            #under sample majority classes
-            if class_freq > 50:
-                class_freq = 50
-            data_weights.append(1/class_freq)
-            
-        sampler = torch.utils.data.sampler.WeightedRandomSampler(weights=data_weights, num_samples=len(self.train_ds))
         data_loader = torch.utils.data.DataLoader(
             self.train_ds,
-            sampler = sampler,
             batch_size=self.config["batch_size"],
-            num_workers=self.config["workers"])
+            shuffle=True,
+            num_workers=self.config["workers"],
+        )
         
         return data_loader
     
