430abd227ee107c5e2d8e2e5664bd4761b68c02a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-03-13 19:55:00 -0700

    smalelr cpu client

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ if config["use_data_commit"]:
 else:
     crop_dir = os.path.join(config["data_dir"], comet_logger.experiment.get_key())
     os.mkdir(crop_dir)
-    client = start_cluster.start(cpus=75, mem_size="5GB")    
+    client = start_cluster.start(cpus=50, mem_size="4GB")    
     config["crop_dir"] = crop_dir
 
 git_branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"]).decode("utf8")[0:-1]
