61cda8fcfe22bea351f61907b058db1697692843
bw4sz <benweinstein2010@gmail.com>
2022-03-18 18:38:24 -0400

    0.1

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ for x in data_module.species_label_dict:
     
 loss_weight = np.array(loss_weight/np.max(loss_weight))
 #Provide min value
-loss_weight[loss_weight < 0.8] = 0.8
+loss_weight[loss_weight < 0.1] = 0.1
 comet_logger.experiment.log_parameter("loss_weight", loss_weight)
 
 m = main.TreeModel(
