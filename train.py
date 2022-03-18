06d915275fdc1aeed6d297d9ba072cadbea2cdd7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-03-18 12:24:43 -0700

    loss weight

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ model = Hang2020.spectral_network(bands=config["bands"], classes=data_module.num
 
 #Loss weight, balanced
 loss_weight = []
-for x in self.species_label_dict:
+for x in data_module.species_label_dict:
     loss_weight.append(1/data_module.train[data_module.train.taxonID==x].shape[0])
     
 loss_weight = loss_weight/loss_weight.max()
