8bb01edbcf7e1634c77bc81deb05fb41ca020415
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-03-19 07:09:27 -0700

    larger crops

--- hand_drawn_crops.py
+++ hand_drawn_crops.py
@@ -10,10 +10,12 @@ import geopandas as gpd
 config = read_config("config.yml")
 test = pd.read_csv(os.path.join(config["data_dir"], config["use_data_commit"], "test.csv"))
 crowns = gpd.read_file(os.path.join(config["data_dir"], config["use_data_commit"], "crowns.shp"))
-crowns = crowns[crowns.individualID.isin(test.individualID)]
+crowns = crowns[crowns.individual.isin(test.individualID)]
 rgb_pool = glob.glob(config["rgb_sensor_pool"], recursive=True)
-for index, row in test.iterrows():
+for index, row in crowns.iterrows():
     sensor_path = neon_paths.find_sensor_path(lookup_pool=rgb_pool, bounds=row["geometry"].bounds)
-    basename = row["individualID"]
-    patches.crop(row["geometry"].bounds, sensor_path, savedir="/orange/idtrees-collab/DeepTreeAttention/hand_annotations/", basename=basename)
+    basename = row["individual"]
+    left, top, right, bottom = row["geometry"].bounds
+    bounds = [left -50, right +50, top + 50, bottom -50]
+    patches.crop(bounds, sensor_path, savedir="/orange/idtrees-collab/DeepTreeAttention/hand_annotations/", basename=basename)
 
