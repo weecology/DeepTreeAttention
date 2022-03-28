6801d1eb18fbcb09ef6a137996aa4d04dd419a3a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-03-28 14:30:48 -0700

    transpose norm

--- src/utils.py
+++ src/utils.py
@@ -33,7 +33,7 @@ def read_config(config_path):
 def preprocess_image(image, channel_is_first=False):
     """Preprocess a loaded image, if already C*H*W set channel_is_first=True"""
     img = np.asarray(image, dtype='float32')
-    data = img.reshape(img.shape[0], np.prod(img.shape[1:]))
+    data = img.reshape(img.shape[0], np.prod(img.shape[1:])).T
     
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', UserWarning)    
