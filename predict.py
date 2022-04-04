310fab056ac224c1f107a643b706efdb5526d0cc
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-04-04 10:40:50 -0700

    test 1 model

--- predict.py
+++ predict.py
@@ -46,7 +46,7 @@ cpu_client = start(cpus=20)
 tif_futures = cpu_client.map(convert, tiles, hyperspectral_pool=hyperspectral_pool, savedir = config["HSI_tif_dir"], year="2019")
 wait(tif_futures)
 
-species_model_dir = "/blue/ewhite/b.weinstein/DeepTreeAttention/102f08bb949b4ba4b5f8b951a47ae519"
+species_model_dir = "/blue/ewhite/b.weinstein/DeepTreeAttention/91ba2dc9445547f48805ec60be0a2f2f"
 dead_model_path = "/orange/idtrees-collab/DeepTreeAttention/Dead/snapshots/c4945ae57f4145948531a0059ebd023c.pl"
 hsi_tifs = []
 for x in tif_futures:
