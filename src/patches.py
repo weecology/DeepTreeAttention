e2bf2b4e806e3be33797a6262af702839ab88083
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-04-04 12:56:12 -0700

    mean centered

--- src/patches.py
+++ src/patches.py
@@ -15,11 +15,9 @@ def crop(bounds, sensor_path, savedir = None, basename = None, mask=None):
         img_tr = src.window_transform(
             window=rasterio.windows.from_bounds(left, bottom, right, top, transform=src.transform)
         )
-        feature_mask = geometry_mask(geometries=[mask], out_shape=img.shape[1:], transform=img_tr, invert=False)
+        feature_mask = geometry_mask(geometries=[mask], out_shape=img.shape[1:], transform=img_tr, invert=True)
         mean_pixel = img.mean(axis=(1,2))
-        feature_mask = feature_mask[:,:] * mean_pixel
-        feature_mask * mean_pixel
-        img = img * feature_mask
+        img = np.where(feature_mask[np.newaxis,:,:,],img,mean_pixel[:,np.newaxis,np.newaxis])        
     if savedir:
         filename = "{}/{}.tif".format(savedir, basename)
         with rasterio.open(filename, "w", driver="GTiff",height=height, width=width, count = img.shape[0], dtype=img.dtype) as dst:
