f5f4b6554eec7422734413e52bf25e5b918bf4a4
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-04-04 11:48:23 -0700

    full prediction

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-#OSBS mining
 from src import predict
 from src import data
 from src import neon_paths
@@ -21,14 +20,14 @@ def find_rgb_files(site, config, year=None):
     
     return tiles
     
-def convert(rgb_path, hyperspectral_pool, year, savedir):
+def convert(rgb_path, hyperspectral_pool, savedir):
     #convert .h5 hyperspec tile if needed
     basename = os.path.basename(rgb_path)
     geo_index = re.search("(\d+_\d+)_image", basename).group(1)
     h5_list = [x for x in hyperspectral_pool if geo_index in x]
     tif_paths = []
     for path in h5_list:
-        year = path[0].split("/")[6]
+        year = path.split("/")[6]
         tif_basename = os.path.splitext(os.path.basename(rgb_path))[0] + "_hyperspectral_{}.tif".format(year)
         tif_path = "{}/{}".format(savedir, tif_basename)
         if not os.path.exists(tif_path):
@@ -43,9 +42,9 @@ tiles = find_rgb_files(site="OSBS", config=config, year=2021)
 hyperspectral_pool = glob(config["HSI_sensor_pool"], recursive=True)
 rgb_pool = glob(config["rgb_sensor_pool"], recursive=True)
 
-cpu_client = start(cpus=60)
+cpu_client = start(cpus=50, mem_size="8GB")
 
-tif_futures = cpu_client.map(convert, tiles, hyperspectral_pool=hyperspectral_pool, savedir = config["HSI_tif_dir"], year=None)
+tif_futures = cpu_client.map(convert, tiles[:1], hyperspectral_pool=hyperspectral_pool, savedir = config["HSI_tif_dir"])
 wait(tif_futures)
 
 species_model_dir = "/blue/ewhite/b.weinstein/DeepTreeAttention/91ba2dc9445547f48805ec60be0a2f2f"
@@ -55,11 +54,12 @@ for x in tif_futures:
     try:
         for path in x.result():
             hsi_tifs.append(path)
-    except:
+    except Exception as e:
+        print(e)
         pass
 
 cpu_client.close()    
-gpu_client = start(gpus=1, mem_size="50GB")
+gpu_client = start(gpus=12, mem_size="10GB")
 
 #No daemonic dask children
 config["workers"] = 0
@@ -74,13 +74,12 @@ except:
 
 geo_index = [re.search("(\d+_\d+)_image", os.path.basename(x)).group(1) for x in hsi_tifs]
 
-for i in pd.Series(geo_index).unique()[:1]:
+for i in pd.Series(geo_index).unique():
     HSI_paths = {}
     tiles = [x for x in hsi_tifs if i in x] 
     for tile in tiles:
         year = os.path.splitext(tile)[0].split("_")[-1]
         HSI_paths[year] = tile
-
     future = gpu_client.submit(
         predict.predict_tile,
         HSI_paths,
