51e4a41ba944447d50526b32029385d7e30db5be
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-05-24 14:51:42 -0700

    skip zeros

--- tests/test_year.py
+++ tests/test_year.py
@@ -7,7 +7,8 @@ import numpy as np
 
 def test_YearEnsemble(dm, config):
     m = year.learned_ensemble(classes=dm.num_classes, years=4, config=config)
-    images = [torch.randn(1, 349, 11, 11) for x in range(4)]
+    images = [torch.randn(1, 349, 11, 11) for x in range(3)]
+    images.append(torch.zeros(1,349,11,11))
     mean_score = m(images)
 
     assert mean_score.shape == (1,dm.num_classes)
