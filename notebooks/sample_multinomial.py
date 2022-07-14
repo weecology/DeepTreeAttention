fb7c807570703db85cedb27abbf5566185cb6c44
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-07-14 11:31:01 -0700

    move multinomial to src

--- notebooks/sample_multinomial.py
+++ notebooks/sample_multinomial.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append("/home/b.weinstein/DeepTreeAttention")
 from src import start_cluster
+from src import multonomial
 client = start_cluster.start(cpus=25)
 
 for x in range(10):
-    wrapper(iteration=x, client=client, savedir="/blue/ewhite/b.weinstein/DeepTreeAttention/results/06ee8e987b014a4d9b6b824ad6d28d83")
+    multonomial.wrapper(iteration=x, client=client, savedir="/blue/ewhite/b.weinstein/DeepTreeAttention/results/06ee8e987b014a4d9b6b824ad6d28d83")
