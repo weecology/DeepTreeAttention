83e5d8ab75fb6b00f864b26e577d4d2c8942084e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-07-30 21:18:32 -0700

    no dask

--- predict.py
+++ predict.py
@@ -54,7 +54,7 @@ config = data.read_config("config.yml")
 comet_logger = CometLogger(project_name="DeepTreeAttention2", workspace=config["comet_workspace"], auto_output_logging="simple")    
 comet_logger.experiment.add_tag("prediction")
 
-gpu_client = start(gpus=7, mem_size="20GB")
+#gpu_client = start(gpus=7, mem_size="20GB")
 species_model_path = "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/06ee8e987b014a4d9b6b824ad6d28d83.pt"
 dead_model_path = "/orange/idtrees-collab/DeepTreeAttention/Dead/snapshots/c4945ae57f4145948531a0059ebd023c.pl"
 config["crop_dir"] = "/blue/ewhite/b.weinstein/DeepTreeAttention/67ec871c49cf472c8e1ae70b185addb1"
@@ -110,24 +110,33 @@ hyperspectral_pool = glob(config["HSI_tif_dir"]+"*")
 tiles = glob("/blue/ewhite/b.weinstein/DeepTreeAttention/results/crowns/*.shp")
 predict_futures = []
 for x in tiles:
-    try:
-        predict_future = gpu_client.submit(predict.predict_tile,
-            crowns=x,
-            img_pool=hyperspectral_pool,
-            filter_dead=True,
-            species_model_path=species_model_path,
-            savedir=prediction_dir,
-            config=config)
-        predict_futures.append(predict_future)
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        continue
+    predict.predict_tile(
+                crowns=x,
+                img_pool=hyperspectral_pool,
+                filter_dead=True,
+                species_model_path=species_model_path,
+                savedir=prediction_dir,
+                config=config)
+            
+#for x in tiles:
+    #try:
+        #predict_future = gpu_client.submit(predict.predict_tile,
+            #crowns=x,
+            #img_pool=hyperspectral_pool,
+            #filter_dead=True,
+            #species_model_path=species_model_path,
+            #savedir=prediction_dir,
+            #config=config)
+        #predict_futures.append(predict_future)
+    #except Exception as e:
+        #print(e)
+        #traceback.print_exc()
+        #continue
 
-for x in predict_futures:
-    try:
-        predicted_trees = x.result()
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        continue        
+#for x in predict_futures:
+    #try:
+        #predicted_trees = x.result()
+    #except Exception as e:
+        #print(e)
+        #traceback.print_exc()
+        #continue        
