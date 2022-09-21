bc54ad2ddd6a4f35c500ecf89f9348bcf12f3cc2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-09-21 11:43:36 -0700

    make a selfsupervised slurm

---
+++ SLURM/selfsupervised.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=Selfsupervised   # Job name
+#SBATCH --mail-type=END               # Mail events
+#SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
+#SBATCH --account=ewhite
+#SBATCH --nodes=1                 # Number of MPI ran
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10GB
+#SBATCH --time=48:00:00       #Time limit hrs:min:sec
+#SBATCH --output=/home/b.weinstein/logs/DeepTreeAttention_%j.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/DeepTreeAttention_%j.err
+#SBATCH --partition=gpu
+
+ulimit -c 0
+
+source activate DeepTreeAttention
+
+cd ~/DeepTreeAttention/
+python notebooks/crop_random_tile.py
