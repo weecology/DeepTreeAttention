ba241a6c839ae9c8120af4b706bfe4d728c80fce
bw4sz <benweinstein2010@gmail.com>
2022-09-30 16:40:24 -0400

    merge

--- sample_multinomial.py
+++ sample_multinomial.py
@@ -4,5 +4,5 @@ import glob
 import pandas as pd
 
 client = start_cluster.start(cpus=50, mem_size="5GB")
-for x in range(2):
+for x in range(100):
     wrapper(client=client, iteration=x, experiment_key="06ee8e987b014a4d9b6b824ad6d28d83")
