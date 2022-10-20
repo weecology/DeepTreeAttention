3d703df30b777fb77be6e027f75b4a96897e498e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-10-20 10:58:49 -0700

    abundance boundary needs correct epsg

--- abundance.py
+++ abundance.py
@@ -5,7 +5,7 @@ import pandas as pd
 import geopandas as gpd
 from src import start_cluster
 
-client = start_cluster.start(cpus=200,mem_size="5GB")
+client = start_cluster.start(cpus=100,mem_size="5GB")
 
 species_model_paths = ["/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/06ee8e987b014a4d9b6b824ad6d28d83.pt",
                        "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/ac7b4194811c4bdd9291892bccc4e661.pt",
@@ -23,11 +23,12 @@ def read_shp(path):
     gdf = gpd.read_file(path)
     #limit by OSBS polygon
     boundary = gpd.read_file("/home/b.weinstein/DeepTreeAttention/data/raw/OSBSBoundary/OSBS_boundary.shp")
+    boundary = boundary.to_crs("epsg:32617")
     intersects = gpd.overlay(gdf, boundary)
     tile_count = intersects.ensembleTa.value_counts()
     
     return tile_count
-        
+
 futures = []
 for species_model_path in species_model_paths:
     print(species_model_path)
@@ -46,7 +47,6 @@ for species_model_path in species_model_paths:
     total_counts.sort_values()
     total_counts.sum()
     total_counts.to_csv("/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/abundance.csv".format(basename))
-    
     
 all_abundance = []
 for species_model_path in species_model_paths:
