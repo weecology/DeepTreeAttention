1aad8be59f6fb6dfb8c0a8c01445c0343132c38b
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-11-02 13:06:01 -0700

    rerun abundance for all data

--- abundance.py
+++ abundance.py
@@ -5,7 +5,7 @@ import pandas as pd
 import geopandas as gpd
 from src import start_cluster
 
-client = start_cluster.start(cpus=70,mem_size="12GB")
+client = start_cluster.start(cpus=75,mem_size="10GB")
 
 ##Same data
 
@@ -34,38 +34,38 @@ def read_shp(path):
     
     return tile_count
 
-#futures = []
-#for species_model_path in species_model_paths:
-    #print(species_model_path)
-    #basename = os.path.splitext(os.path.basename(species_model_path))[0]
-    #input_dir = "/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/*_image.shp".format(basename)
-    #files = glob(input_dir)
-    #print(files)
-    #if len(files) == 0:
-        #continue
-    #counts = []
-    #futures = client.map(read_shp,files)
-    #counts = [x.result() for x in futures]
-    #total_counts = pd.Series()
-    #for ser in counts:
-        #total_counts = total_counts.add(ser, fill_value=0)
-    #total_counts.sort_values()
-    #total_counts.sum()
-    #total_counts.to_csv("/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/abundance.csv".format(basename))
+futures = []
+for species_model_path in species_model_paths:
+    print(species_model_path)
+    basename = os.path.splitext(os.path.basename(species_model_path))[0]
+    input_dir = "/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/*_image.shp".format(basename)
+    files = glob(input_dir)
+    print(files)
+    if len(files) == 0:
+        continue
+    counts = []
+    futures = client.map(read_shp,files)
+    counts = [x.result() for x in futures]
+    total_counts = pd.Series()
+    for ser in counts:
+        total_counts = total_counts.add(ser, fill_value=0)
+    total_counts.sort_values()
+    total_counts.sum()
+    total_counts.to_csv("/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/abundance.csv".format(basename))
     
-#all_abundance = []
-#for species_model_path in species_model_paths:
-    #basename = os.path.splitext(os.path.basename(species_model_path))[0]    
-    #try:
-        #df = pd.read_csv("/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/abundance.csv".format(basename))
-    #except:
-        #continue
-    #df["path"] = basename
-    #all_abundance.append(df)
+all_abundance = []
+for species_model_path in species_model_paths:
+    basename = os.path.splitext(os.path.basename(species_model_path))[0]    
+    try:
+        df = pd.read_csv("/blue/ewhite/b.weinstein/DeepTreeAttention/results/{}/abundance.csv".format(basename))
+    except:
+        continue
+    df["path"] = basename
+    all_abundance.append(df)
 
-#all_abundance = pd.concat(all_abundance)
-#all_abundance.columns = ["taxonID","count","model"]
-#all_abundance.to_csv("results/cross_validation_abundance.csv")
+all_abundance = pd.concat(all_abundance)
+all_abundance.columns = ["taxonID","count","model"]
+all_abundance.to_csv("results/cross_validation_abundance.csv")
 
 species_model_paths = ["/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/06ee8e987b014a4d9b6b824ad6d28d83.pt",
                        "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/ac7b4194811c4bdd9291892bccc4e661.pt",
