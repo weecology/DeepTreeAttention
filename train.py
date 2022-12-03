e1e59595d1dcda38020caa0c177a654262667b25
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-12-02 17:14:14 -0800

    specify config

--- train.py
+++ train.py
@@ -86,6 +86,7 @@ def main():
     
     m = baseline.TreeModel(
         model=model, 
+        config=config
         classes=data_module.num_classes, 
         loss_weight=loss_weight,
         label_dict=data_module.species_label_dict)
