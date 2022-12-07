5e27c70ac05dee73c4d52279f6e2b8e63473dc4d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-12-07 15:05:23 -0800

    place outside init

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -152,26 +152,6 @@ class TreeModel(LightningModule):
         
         return pred
     
-    def train_dataloader(self):
-        data_loader = torch.utils.data.DataLoader(
-            self.trainer.datamodule.train_ds,
-            batch_size=self.config["batch_size"],
-            shuffle=True,
-            num_workers=self.config["workers"],
-        )
-        
-        return data_loader
-    
-    def val_dataloader(self):
-        data_loader = torch.utils.data.DataLoader(
-            self.trainer.datamodule.val_ds,
-            batch_size=self.config["batch_size"],
-            shuffle=False,
-            num_workers=self.config["workers"],
-        )
-        
-        return data_loader
-        
     def predict_dataloader(self, data_loader, test_crowns=None, test_points=None, plot_n_individuals=1, return_features=False, experiment=None, train=True):
         """Given a file with paths to image crops, create crown predictions 
         The format of image_path inform the crown membership, the files should be named crownid_counter.png where crownid is a
