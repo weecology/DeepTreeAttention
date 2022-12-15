179418f08c0cfb2f78b94a19a547f79033971a7f
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-12-15 10:24:20 -0800

    simplify lr parameters

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -86,14 +86,8 @@ class TreeModel(LightningModule):
     def configure_optimizers(self):
         optimizer = optim.Adam(self.model.parameters(), lr=self.config["lr"])
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
-                                                         mode='min',
-                                                         factor=0.75,
-                                                         patience=2,
-                                                         verbose=True,
-                                                         threshold=0.001,
-                                                         threshold_mode='rel',
-                                                         cooldown=0,
-                                                         eps=1e-08)
+                                                         factor=0.5,
+                                                         patience=5)
                                                                  
         return {'optimizer':optimizer, 'scheduler': scheduler,"monitor":'val_loss',"frequency":self.config["validation_interval"], "interval": "epoch"}
     
