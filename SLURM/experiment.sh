bf5cb05aba04604a089d84d1814b18fd32429df3
bw4sz <benweinstein2010@gmail.com>
2022-12-20 17:13:06 -0500

    fewer workers

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=250GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepTreeAttention_%j.out   # Standard output and error log
