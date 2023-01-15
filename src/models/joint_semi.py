7b9fed18ebeb1a2eec18267a034910fbc95578f3
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-01-14 21:30:19 -0800

    revert back to best performing branch and checkout the important semi supervised files for refactoring

---
+++ src/models/joint_semi.py
@@ -0,0 +1,87 @@
+#Lightning Data Module
+from . import __file__
+import numpy as np
+from pytorch_lightning import LightningModule
+import os
+import pandas as pd
+from torch.nn import functional as F
+from torch import optim
+import torch
+import torchmetrics
+import copy
+
+from src import utils
+from src.models import multi_stage
+from src import data, semi_supervised
+
+class TreeModel(multi_stage.MultiStage):
+    """A pytorch lightning data module
+    Args:
+        model (str): Model to use. See the models/ directory. The name is the filename, each model should take in the same data loader
+    """
+    def __init__(self, train_df, test_df, taxonomic_csv, config=None):
+        super().__init__(train_df=train_df, test_df=test_df, taxonomic_csv=taxonomic_csv, config=config)
+    
+        # Unsupervised versus supervised loss weight
+        self.alpha = torch.nn.Parameter(torch.tensor(self.config["semi_supervised"]["alpha"], dtype=float), requires_grad=False)
+        if self.config["semi_supervised"]["semi_supervised_train"] is None:
+            self.semi_supervised_train = semi_supervised.create_dataframe(config, label_to_taxon_id=self.index_to_label)
+        else:
+            self.semi_supervised_train = pd.read_csv(self.config["semi_supervised"]["semi_supervised_train"])
+        
+        print("Shape of semi-supervised {}".format(self.semi_supervised_train.shape))
+        
+        # Hierarchical psuedolabels
+        self.psuedo_dataframes, _ = self.create_levels(self.semi_supervised_train, level_label_dicts=self.level_label_dicts)
+        
+    def train_dataloader(self):
+        semi_supervised_config = copy.deepcopy(self.config)
+        semi_supervised_config["crop_dir"] = semi_supervised_config["semi_supervised"]["crop_dir"]
+        semi_supervised_config["preload_images"] = semi_supervised_config["semi_supervised"]["preload_images"]
+        semi_supervised_config["workers"] = semi_supervised_config["semi_supervised"]["workers"]
+
+        train_dataloaders = []
+        for level, ds in enumerate(self.train_datasets):
+            # labeled 
+            labeled_data_loader = torch.utils.data.DataLoader(
+                ds,
+                batch_size=self.config["batch_size"],
+                shuffle=True,
+                num_workers=self.config["workers"],
+            )
+            
+            # Unlabeled
+            pseudo_dataset = self.TreeDataset(df=self.psuedo_dataframes[level], config=semi_supervised_config)
+            unlabeled_data_loader = torch.utils.data.DataLoader(
+                pseudo_dataset,
+                batch_size=self.config["batch_size"],
+                shuffle=True,
+                num_workers=self.config["workers"],
+            )
+            train_dataloaders.append({"labeled":labeled_data_loader, "unlabeled": unlabeled_data_loader})
+        
+        return train_dataloaders
+    
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        """Train on a loaded dataset
+        """
+        
+        # Labeled data
+        loss_weights = self.__getattr__('loss_weight_'+str(optimizer_idx))        
+        individual, inputs, y = batch[optimizer_idx]["labeled"]
+        images = inputs["HSI"]
+        y_hat = self.models[optimizer_idx].forward(images)
+        supervised_loss = F.cross_entropy(y_hat, y, weight=loss_weights)   
+        
+        # Unlabeled data
+        individual, inputs, y = batch[optimizer_idx]["unlabeled"]
+        images = inputs["HSI"]
+        y_hat = self.models[optimizer_idx].forward(images)        
+        unsupervised_loss = F.cross_entropy(y_hat, y)    
+        
+        self.log("supervised_loss_level_{}".format(optimizer_idx),supervised_loss)
+        self.log("unsupervised_loss_level_{}".format(optimizer_idx), unsupervised_loss)
+        self.log("alpha", self.alpha, on_step=False, on_epoch=True)
+        loss = supervised_loss + self.alpha * unsupervised_loss 
+        
+        return loss
---
+++ src/semi_supervised.py
@@ -0,0 +1,136 @@
+#Prepare semi_supervised data
+import glob
+import torch
+import copy
+import torch
+import random
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from pytorch_lightning import Trainer
+
+from src.data import TreeDataset
+from src.models import baseline
+
+def load_unlabeled_data(config, client=None):
+    semi_supervised_crops_csvs = glob.glob("{}/*.shp".format(config["semi_supervised"]["crop_dir"]))
+    
+    if len(semi_supervised_crops_csvs) == 0:
+        raise ValueError("No .shp files found in {}".format(config["semi_supervised"]["crop_dir"]))
+    
+    random.shuffle(semi_supervised_crops_csvs)
+    semi_supervised_crops_csvs = semi_supervised_crops_csvs[:config["semi_supervised"]["limit_shapefiles"]]
+    if client:
+        semi_supervised_crops = client.map(gpd.read_file, semi_supervised_crops_csvs)
+        semi_supervised_crops = client.gather(semi_supervised_crops)
+    else:
+        semi_supervised_crops = [gpd.read_file(x) for x in semi_supervised_crops_csvs]
+        
+    semi_supervised_crops = pd.concat(semi_supervised_crops)
+    
+    #if present remove dead trees
+    try:
+        semi_supervised_crops = semi_supervised_crops[semi_supervised_crops.dead_label==0]
+    except:
+        pass
+    
+    if config["semi_supervised"]["site_filter"] is not None:
+        site_semi_supervised_crops = semi_supervised_crops[semi_supervised_crops.image_path.str.contains(config["semi_supervised"]["site_filter"])]
+    else:
+        site_semi_supervised_crops = semi_supervised_crops
+    
+    site_semi_supervised_crops = site_semi_supervised_crops.sample(frac=1)
+    individuals_to_keep = pd.Series(site_semi_supervised_crops.individual.unique()).sample(n=config["semi_supervised"]["num_samples"])    
+    site_semi_supervised_crops = site_semi_supervised_crops[site_semi_supervised_crops.individual.isin(individuals_to_keep)]
+    
+    # Reset the index to 0:n_samples
+    site_semi_supervised_crops = site_semi_supervised_crops.reset_index(drop=True)
+    
+    # All individuals need atleast 2 years of day
+    counts = site_semi_supervised_crops.individual.value_counts()
+    individuals_to_keep = counts[~(counts == 1)].index.values
+    site_semi_supervised_crops = site_semi_supervised_crops[site_semi_supervised_crops.individual.isin(individuals_to_keep)]
+    
+    return site_semi_supervised_crops
+    
+def select_samples(predicted_samples, config):
+    """Given a unlabeled dataframe, select which samples to include in dataloader"""
+    samples_to_keep = predicted_samples[predicted_samples.score > config["semi_supervised"]["threshold"]]
+    
+    #Optionally balance and limit
+    individuals_to_keep = samples_to_keep.groupby("taxonID").apply(lambda x: x.drop_duplicates(subset="individual").sample(frac=1).head(n=config["semi_supervised"]["max_samples_per_class"])).individual.values
+    predicted_samples = predicted_samples[predicted_samples.individual.isin(individuals_to_keep)].reset_index(drop=True)
+
+    return predicted_samples
+
+def predict_unlabeled(config, annotation_df, label_to_taxon_id, m=None):
+    """Predict unlabaled data with model in memory or loaded from file
+    Args:
+        config: a DeepTreeAttention config
+        annotation_df: a pandas dataframe with image_path, to be into data.TreeDataset
+    Returns:
+        ensemble_df: ensembled dataframe of predictions
+    """
+    
+    if annotation_df.empty:
+        raise ValueError("Annoatation Dataframe has no rows")
+    
+    new_config = copy.deepcopy(config)
+    new_config["crop_dir"] = new_config["semi_supervised"]["crop_dir"]
+    new_config["workers"] = new_config["semi_supervised"]["workers"]
+    new_config["preload_images"] = new_config["semi_supervised"]["preload_images"]
+    
+    if m is None:
+        m = baseline.TreeModel.load_from_checkpoint(new_config["semi_supervised"]["model_path"], config=new_config)
+    
+    trainer = Trainer(gpus=new_config["gpus"], logger=False, enable_checkpointing=False)
+    ds = TreeDataset(df = annotation_df, train=False, config=new_config)
+    data_loader = torch.utils.data.DataLoader(
+        ds,
+        batch_size=new_config["predict_batch_size"],
+        shuffle=False,
+        num_workers=new_config["workers"],
+    )
+    
+    predictions = trainer.predict(m, dataloaders=data_loader)  
+    
+    if predictions is None:
+        raise ValueError("No predictions made from the trainer dataloader")
+    
+    predictions = np.vstack(predictions)
+    annotation_df["label"] = np.argmax(predictions,axis=1)
+    annotation_df["score"] = np.max(predictions,axis=1) 
+    annotation_df["taxonID"] = annotation_df.label.apply(lambda x: label_to_taxon_id[x])
+    
+    return annotation_df
+
+def create_dataframe(config, label_to_taxon_id, unlabeled_df=None, m=None, client=None):
+    """Generate a pytorch dataloader from unlabeled crop data"""
+    
+    if unlabeled_df is None:
+        unlabeled_df = load_unlabeled_data(config, client=client)
+        
+    predicted_samples = predict_unlabeled(config, unlabeled_df, label_to_taxon_id=label_to_taxon_id, m=m)
+    
+    # Predict labels for each crop
+    selected_df = select_samples(
+        predicted_samples=predicted_samples,
+        config=config
+    )
+    
+    return selected_df
+
+def create_dataloader(unlabeled_df, config):
+    semi_supervised_ds = TreeDataset(
+        df=unlabeled_df,
+        train=False,
+        config=config)
+    
+    data_loader = torch.utils.data.DataLoader(
+        semi_supervised_ds,
+        batch_size=config["batch_size"],
+        shuffle=True,
+        num_workers=config["workers"]
+    )        
+    
+    return data_loader
