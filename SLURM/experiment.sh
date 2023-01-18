8f289e436c534bc1af7d1ab2fae7bbad58f3d0a3
bw4sz <benweinstein2010@gmail.com>
2023-01-18 12:49:06 -0500

    more worker memory

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
 #SBATCH --cpus-per-task=5
-#SBATCH --mem=200GB
+#SBATCH --mem=150GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepTreeAttention_%j.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/DeepTreeAttention_%j.err
