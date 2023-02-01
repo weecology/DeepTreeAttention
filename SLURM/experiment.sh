474b57231bd762ce8308f2c10a8cf313296fc161
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-01-31 19:53:11 -0800

    All sites data

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ran
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=250GB
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=50GB
 #SBATCH --time=48:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepTreeAttention_%j.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/DeepTreeAttention_%j.err
