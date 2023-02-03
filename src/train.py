ec1ec63322a041f33702dbb16a5d60f8f22fec52
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-02-03 14:57:20 -0800

    confusion label

--- src/train.py
+++ src/train.py
@@ -90,7 +90,7 @@ def train_model(train, test, data_module, comet_logger, name):
         
         comet_logger.experiment.log_confusion_matrix(
             results.label.values,
-            results.pred_taxa_top1.values,
+            results.pred_label_top1.values,
             labels=list(data_module.species_label_dict.keys()),
             max_categories=len(data_module.species_label_dict.keys()),
             title=name
