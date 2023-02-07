4e83da89d498442d8479e2ed9454c1badf480e6d
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-02-07 10:57:28 -0800

    All site pretrain

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         train = df.reset_index(drop=True)
         train.to_csv("{}/train_{}_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], config["train_test_commit"]))
         
-        files = glob.glob("*{}*".format(config["commit hash"]))
+        files = glob.glob("*{}*".format(config["train_test_commit"]))
         files = [x for x in files if "test" in x]
         dfs = [pd.read_csv(x) for x in files]
         df = pd.concat(dfs)
