452fd427bb6c72aeb54dc4f59dfed24eaa4313bb
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-02-07 10:31:11 -0800

    All site pretrain

--- train.py
+++ train.py
@@ -16,18 +16,18 @@ if __name__ == "__main__":
         dfs = [pd.read_csv(x) for x in files]
         df = pd.concat(dfs)
         train = df.reset_index(drop=True)
-        train.to_csv("{}/train_{}_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], config["commit_hash"]))
+        train.to_csv("{}/train_{}_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], git_commit))
         
         files = glob.glob("*{}*".format(config["commit hash"]))
         files = [x for x in files if "test" in x]
         dfs = [pd.read_csv(x) for x in files]
         df = pd.concat(dfs)
         test = df.reset_index(drop=True)
-        test.to_csv("{}/test_{}_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], config["commit_hash"]))
+        test.to_csv("{}/test_{}_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], git_commit))
         
         site = "pretrain"        
         train.main(git_branch, git_commit, config, site)
-        config["pretrain_state_dict"] = "{}_{}_pretrain_state_dict.pl".format(config["snapshot_dir"],config["commit_hash"])
+        config["pretrain_state_dict"] = "{}_{}_pretrain_state_dict.pl".format(config["snapshot_dir"],git_commit)
         
     for site in ["BART","BLAN", "BONA","CLBJ", "DEJU", "DELA", "GRSM", "HARV", "JERC",
                   "LENO", "MLBS", "MOAB", "NIWO" ,"OSBS","RMNP","SCBI","SERC","SJER","SOAP",
