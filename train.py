25510630c328cb6f3378ae8f97bf53378ce3e05b
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-02-07 13:48:56 -0800

    back to naming experiment id

--- train.py
+++ train.py
@@ -9,26 +9,20 @@ if __name__ == "__main__":
     git_branch=sys.argv[1]
     git_commit=sys.argv[2]    
     config = data.read_config("config.yml")
-    
+    original_commit = config["train_test_commit"].copy()
     if config["all_site_pretrain"]:
-        files = glob.glob("{}/{}/*{}*".format(config["data_dir"],config["use_data_commit"],config["train_test_commit"]))
-        files = [x for x in files if "train" in x]
-        dfs = [pd.read_csv(x) for x in files]
-        df = pd.concat(dfs)
-        traindf = df.reset_index(drop=True)
-        traindf.to_csv("{}/{}/train_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], config["train_test_commit"]))
-        
         files = glob.glob("{}/{}/*{}*".format(config["data_dir"],config["use_data_commit"],config["train_test_commit"]))
         files = [x for x in files if "test" in x]
         dfs = [pd.read_csv(x) for x in files]
         df = pd.concat(dfs)
         testdf = df.reset_index(drop=True)
-        testdf.to_csv("{}/{}/test_{}_pretrain.csv".format(config["data_dir"],config["use_data_commit"], config["train_test_commit"]))
-        
-        site = "pretrain"        
+        testdf.to_csv("{}/{}/test_{}_all.csv".format(config["data_dir"],config["use_data_commit"], config["train_test_commit"]))
+        site = "all"
+        config["existing_test_csv"] = "{}/{}/test_{}_all.csv".format(config["data_dir"],config["use_data_commit"], config["train_test_commit"])
+        config["train_test_commit"] = None
         comet_logger = train.main(git_branch, git_commit, config, site)
-        config["pretrain_state_dict"] = "{}/{}_pretrain_state_dict.pl".format(config["snapshot_dir"],comet_logger.experiment.id)
-        
+        config["pretrain_state_dict"] = "{}/{}_all_state_dict.pl".format(config["snapshot_dir"],comet_logger.experiment.id)
+        config["train_test_commit"] = original_commit
     for site in ["BART","BLAN", "BONA","CLBJ", "DEJU", "DELA", "GRSM", "HARV", "JERC",
                   "LENO", "MLBS", "MOAB", "NIWO" ,"OSBS","RMNP","SCBI","SERC","SJER","SOAP",
                  "STEI","TALL","TEAK","TREE","UKFS","UNDE","WREF","YELL"]:
