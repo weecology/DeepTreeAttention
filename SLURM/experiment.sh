751fa8f8f7b95af01c9c9b01ca3d803bc66208d8
bw4sz <benweinstein2010@gmail.com>
2023-03-10 16:51:00 -0500

    cuda error

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -25,5 +25,5 @@ cd ~/DeepTreeAttention/
 #get branch and commit name
 branch_name=$((git symbolic-ref HEAD 2>/dev/null || echo "(unnamed branch)")|cut -d/ -f3-)
 commit=$(git log --pretty=format:'%H' -n 1)
-export COMET_LOGGING_CONSOLE=info
+CUDA_LAUNCH_BLOCKING=1
 python train.py $branch_name $commit $2
