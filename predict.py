d7317368f9c5bb2464e877ded394f40cc8c628b2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-04-11 13:30:42 -0700

    new models

--- predict.py
+++ predict.py
@@ -51,11 +51,11 @@ config["crop_dir"] = "/blue/ewhite/b.weinstein/DeepTreeAttention/results/site_cr
 savedir = config["crop_dir"] 
 
 species_model_paths = {
-    #"NIWO": "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/234a38632f2142beb82e7d0ad701e4f7_['NIWO', 'RMNP'].pt",
-    #"SJER":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/0c580b6730614574bc232245422a2600_['SJER'].pt",
-    #"MOAB":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/7c7abe8e0b0040e1b7d7a62c1d8926e5_['MOAB', 'REDB'].pt",
-    #"WREF":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/ab9eee4c0c6b44ce9a6fda25beab8e83_['WREF'].pt",
-    #"REDB":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/7c7abe8e0b0040e1b7d7a62c1d8926e5_['MOAB', 'REDB'].pt",
+    #"NIWO": "/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/4a962f4745204a82b3688ed505cd76d8_['NIWO', 'RMNP'].pt",
+    #"SJER":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/87138a0b383c4dfea2df8fb3d6e48119_['SJER'].pt",
+    #"MOAB":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/54db5e883404420a95af36787f4395d3_['MOAB', 'REDB'].pt",
+    #"WREF":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/f0f2e7eb0e33484dadcfa011bc6ac745_['WREF'].pt",
+    #"REDB":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/54db5e883404420a95af36787f4395d3_['MOAB', 'REDB'].pt",
     #"SERC":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/cd1739965ac54da781b9cbc89ed4f131_['SERC', 'GRSM'].pt",
     #"GRSM":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/cd1739965ac54da781b9cbc89ed4f131_['SERC', 'GRSM'].pt",
     #"DEJU":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/da0d5a8500e54f0599bce7876e397f89_['BONA', 'DEJU'].pt",
