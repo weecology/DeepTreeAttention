13bba61f2fe69b13b37c099d702db6efc1b6692e
bw4sz <benweinstein2010@gmail.com>
2023-04-19 14:37:49 -0400

    lower flat model lr

--- SLURM/cross_site_wrapper.sh
+++ SLURM/cross_site_wrapper.sh
@@ -1,7 +1,7 @@
 #cross_site_wrapper
 #!/bin/bash
-SITES='["MLBS"_"BLAN"_"UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"_"GRSM"] ["NIWO"_"RMNP"] ["WREF"] ["SJER"] ["BONA"_"DEJU"] ["GUAN"] ["TREE"_"STEI"_"UNDE"] ["DELA"_"LENO"] ["CLBJ"] ["TEAK"_"SOAP"_"YELL"] ["MOAB"_"REDB"] ["OSBS"_"JERC"_"TALL"_"DSNY"]'
-#SITES='["BART"]'
+#SITES='["MLBS"_"BLAN"_"UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"_"GRSM"] ["NIWO"_"RMNP"] ["WREF"] ["SJER"] ["BONA"_"DEJU"] ["GUAN"] ["TREE"_"STEI"_"UNDE"] ["DELA"_"LENO"] ["CLBJ"] ["TEAK"_"SOAP"_"YELL"] ["MOAB"_"REDB"] ["OSBS"_"JERC"_"TALL"_"DSNY"]'
+SITES='["DELA"] ["BLAN"]'
 for SITE in $SITES; do
 echo "${SITE}"
 sbatch SLURM/experiment.sh $1 ${SITE}
