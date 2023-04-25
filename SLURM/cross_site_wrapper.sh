c555c9ac0c05bf86fcb42b67f8e48dd25a4e212f
bw4sz <benweinstein2010@gmail.com>
2023-04-25 13:49:57 -0400

    focus on BART tests

--- SLURM/cross_site_wrapper.sh
+++ SLURM/cross_site_wrapper.sh
@@ -1,7 +1,7 @@
 #cross_site_wrapper
 #!/bin/bash
-SITES='["MLBS"] ["BLAN"] ["UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"] ["GRSM"] ["NIWO"] ["RMNP"] ["WREF"] ["SJER"] ["BONA"] ["DEJU"] ["GUAN"] ["TREE"] ["STEI"] ["UNDE"] ["DELA"] ["LENO"] ["CLBJ"] ["TEAK"] ["SOAP"] ["YELL"] ["MOAB"] ["REDB"] ["OSBS"] ["JERC"] ["TALL"] ["DSNY"]'
-#SITES='["BLAN"] ["BART"] ["SCBI"] ["UKFS"]'
+#SITES='["MLBS"] ["BLAN"] ["UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"] ["GRSM"] ["NIWO"] ["RMNP"] ["WREF"] ["SJER"] ["BONA"] ["DEJU"] ["GUAN"] ["TREE"] ["STEI"] ["UNDE"] ["DELA"] ["LENO"] ["CLBJ"] ["TEAK"] ["SOAP"] ["YELL"] ["MOAB"] ["REDB"] ["OSBS"] ["JERC"] ["TALL"] ["DSNY"]'
+SITES='["BART"]'
 for SITE in $SITES; do
 echo "${SITE}"
 sbatch SLURM/experiment.sh $1 ${SITE}
