561cdad8a4ea1bb3344055021519c6fc3d1a9e65
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-05-10 14:51:22 -0700

    BLAN prediction

--- SLURM/cross_site_wrapper.sh
+++ SLURM/cross_site_wrapper.sh
@@ -4,5 +4,5 @@ SITES='MLBS BLAN UKFS SCBI BART HARV SERC GRSM NIWO RMNP WREF SJER BONA DEJU GUA
 for SITE in $SITES; do
 echo "${SITE}"
 sbatch SLURM/experiment.sh $1 ${SITE}
-sleep 1 # pause to be kind to the scheduler
+sleep 10 # pause to be kind to the scheduler
 done
