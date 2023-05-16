6e4dccab897238b235d1514e51f6112e498acbbc
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2023-05-16 13:31:23 -0700

    more flexibility in data location

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ if config["use_data_commit"] is None:
         config=config,
         client=client,
         create_train_test=True,
-        site="all",
+        site="pretrain",
         comet_logger=comet_logger)
     config["use_data_commit"] = comet_logger.experiment.id 
 elif config["train_test_commit"] is None:
