69109292066ad30c1480a050ba29dac0a40e02ea
bw4sz <benweinstein2010@gmail.com>
2023-05-18 09:47:05 -0400

    train longer

--- SLURM/cross_site_wrapper.sh
+++ SLURM/cross_site_wrapper.sh
@@ -1,7 +1,7 @@
 #cross_site_wrapper
 #!/bin/bash
-#SITES='["MLBS"] ["BLAN"] ["UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"] ["GRSM"] ["NIWO"] ["RMNP"] ["WREF"] ["SJER"] ["BONA"] ["DEJU"] ["GUAN"] ["TREE"] ["STEI"] ["UNDE"] ["DELA"] ["LENO"] ["CLBJ"] ["TEAK"] ["SOAP"] ["YELL"] ["MOAB"] ["REDB"] ["OSBS"] ["JERC"] ["TALL"] ["DSNY"]'
-SITES='["GRSM"]'
+SITES='["MLBS"] ["BLAN"] ["UKFS"] ["SCBI"] ["BART"] ["HARV"] ["SERC"] ["GRSM"] ["NIWO"] ["RMNP"] ["WREF"] ["SJER"] ["BONA"] ["DEJU"] ["GUAN"] ["TREE"] ["STEI"] ["UNDE"] ["DELA"] ["LENO"] ["CLBJ"] ["TEAK"] ["SOAP"] ["YELL"] ["MOAB"] ["REDB"] ["OSBS"] ["JERC"] ["TALL"] ["DSNY"]'
+#SITES='["GRSM"]'
 for SITE in $SITES; do
 echo "${SITE}"
 sbatch SLURM/experiment.sh $1 ${SITE}
