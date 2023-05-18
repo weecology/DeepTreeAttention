68d23c6748da8d5686371b782616c80bc44fa175
bw4sz <benweinstein2010@gmail.com>
2023-05-18 14:35:14 -0400

    new train test split

--- SLURM/predict_wrapper.sh
+++ SLURM/predict_wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SITES="OSBS JERC MLBS BLAN UKFS SCBI BART HARV SERC GRSM NIWO RMNP WREF SJER BONA DEJU TREE STEI UNDE DELA LENO CLBJ TEAK SOAP YELL OSBS JERC TALL"
-SITES="GRSM OSBS"
+SITES="UNDE OSBS"
 for SITE in $SITES; do
 echo "${SITE}"
 sbatch SLURM/predict.sh ${SITE}
-sleep 300 # pause to be kind to the scheduler
+sleep 100 # pause to be kind to the scheduler
 done
