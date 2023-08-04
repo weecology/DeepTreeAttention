6cf2f8701be0084b87aa9bf85636efe90a222273
bw4sz <benweinstein2010@gmail.com>
2023-08-04 19:27:07 -0400

    back to size 30

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -2,10 +2,12 @@
 import numpy as np
 from src import augmentation
 import torch
+import pytest
 
-def test_train_augmentation():
+@pytest.mark.parametrize("train",[True,False])
+def test_train_augmentation(train):
     image = torch.randn(20, 369, 11, 11)    
-    transformer = augmentation.augment(image_size=11)
+    transformer = augmentation.augment(image_size=11, train=train)
     transformed_image = transformer(image)
     assert transformed_image.shape == image.shape
     assert not np.array_equal(image, transformed_image)
