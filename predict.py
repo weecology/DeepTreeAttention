186a1a4e11189755057427c282e60606bdf0a207
bw4sz <benweinstein2010@gmail.com>
2023-08-10 20:00:46 -0400

    STEI model

--- predict.py
+++ predict.py
@@ -71,7 +71,7 @@ species_model_paths = {
     "DEJU":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/f96e4b931e684230bb1e6e98ac6c302b_DEJU.pt",
     "BONA":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/152a61614a4a48cf84b27f5880692230_BONA.pt",
     "TREE":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/990e6f1101b2423f86d4cd16f373deab_TREE.pt",
-    "STEI":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/e5581d389ff34dbeb406261a6e512141_STEI.pt",
+    "STEI":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/35220561e5834d03b1d098c84a00a171_STEI.pt",
     "UNDE":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/a6dc9a627a7446acbc40c5b7913a45e9_UNDE.pt",
     "DELA":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/5d3578860bfd4fd79072de872452ea79_DELA.pt",
     "LENO":"/blue/ewhite/b.weinstein/DeepTreeAttention/snapshots/537b80fd46e64c77a1c367dcbef713e3_LENO.pt",
